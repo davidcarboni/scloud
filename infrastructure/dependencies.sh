ed514fe49790e68af125c47ef0aac71213ce7382
David Carboni <david@carboni.io>
2022-05-25 22:34:12 +0100

    package.json updates

---
+++ infrastructure/dependencies.sh
@@ -0,0 +1,9 @@
+
+npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb-base eslint-plugin-import
+npm i @octokit/rest tweetsodium
+
+#   "scripts": {
+#    ...,
+#    "lint": "eslint --fix --ext ts bin lib",
+#    "secrets": "ts-node src/github/secrets.ts"
+
