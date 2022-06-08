b592fde2ce96613374f5eb356e0a24a2e3566b00
David Carboni <david@carboni.io>
2022-06-08 16:00:03 +0100

    Simplify

--- infrastructure/dependencies.sh
+++ infrastructure/dependencies.sh
@@ -1,6 +1,5 @@
 
-npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb-base eslint-plugin-import eslint-import-resolver-typescript
-npm i --save-dev @types/node
+npm i --save-dev @types/node @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb-base eslint-plugin-import eslint-import-resolver-typescript
 npm i @octokit/rest tweetsodium
 
 #   "scripts": {
