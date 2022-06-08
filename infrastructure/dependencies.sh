5030cf59b3297df9a880031d6549724ae2217c98
David Carboni <david@carboni.io>
2022-06-08 15:59:22 +0100

    Missing eslint import resolver

--- infrastructure/dependencies.sh
+++ infrastructure/dependencies.sh
@@ -1,5 +1,6 @@
 
-npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb-base eslint-plugin-import
+npm i --save-dev @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb-base eslint-plugin-import eslint-import-resolver-typescript
+npm i --save-dev @types/node
 npm i @octokit/rest tweetsodium
 
 #   "scripts": {
