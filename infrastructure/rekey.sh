f94badcc8c34b7141476311ef23ae367d9868779
David Carboni <david@carboni.io>
2022-06-17 20:53:02 +0100

    Source secrets and AWS profile for rekey

--- infrastructure/rekey.sh
+++ infrastructure/rekey.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -eu
-export AWS_PROFILE=carboni
 
-source ../secrets/google.sh
-source ../secrets/facebook.sh
-source ../secrets/github.sh
-source ../secrets/slack.sh
+# Secrets, including selected AWS profile:
+for i in $(ls ../secrets/*.sh); do
+  echo " - $i"
+  source $i
+done
+if [ -f '../secrets/aws.sh' ]; then
+  echo "Using AWS profile: $AWS_PROFILE"
+else
+  echo "Using default AWS profile"
+fi
 
 npm run lint
 REKEY=true cdk deploy
