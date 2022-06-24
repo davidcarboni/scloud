f5e68f2b000eb9d9c5928417fdc2600009eec81f
David Carboni <david@carboni.io>
2022-06-25 00:42:39 +0100

    Lambda to procedd messages from a queue and return any failures for retry

---
+++ queue-lambda/test/lambda_function.spec.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import * as chai from 'chai';
+import { describe, it } from 'mocha';
+import * as lambda from '../src/lambda';
+
+chai.use(require('chai-as-promised'));
+
+describe('Handle messages', () => {
+  it('Should handle a messge', () => {
+    // ...
+  });
+});
