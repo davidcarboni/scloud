48341d4def356f160d5739a0bf0b9bb41950202a
David Carboni <david@carboni.io>
2022-06-29 14:39:19 +0100

    Simplified down to two functions

--- slack/src/lambda.ts
+++ slack/src/lambda.ts
@@ -1,60 +1,49 @@
 import {
-  Context, SQSBatchResponse, SQSEvent, SQSRecord,
+  Context, SQSBatchResponse, SQSEvent,
 } from 'aws-lambda';
 import axios from 'axios';
 import * as fs from 'fs';
 
-const slackWebhook = process.env.SLACK_WEBHOOK || '';
-
-// Provided by the container/environment/file
+// The version of the code we're running
 let commitHash = process.env.COMMIT_HASH || 'development';
 if (fs.existsSync('COMMIT_HASH')) {
   commitHash = fs.readFileSync('COMMIT_HASH').toString();
 }
 
-// Any failed messages for retry
-const failedMessageIds: string[] = [];
-
+/**
+ * Process the content of an SQS message
+ */
 export async function processMessage(body: string) {
+  const slackWebhook = process.env.SLACK_WEBHOOK || '';
   if (slackWebhook) {
     await axios.post(slackWebhook, { text: `${body}` });
   } else {
-    console.log(`Slack message (mot sent): ${body}`);
+    console.log(`Slack message (not sent): ${body}`);
   }
 }
 
 /**
- * Process an SQS messagee
- * @param record A received message.
- */
-export async function processRecord(record: SQSRecord) {
-  try {
-    processMessage(record.body);
-  } catch (err) {
-    // Note this item for retry
-    failedMessageIds.push(record.messageId);
-    console.log(`Message error: ${err} [${record.messageId}]`);
-  }
-}
-
-/**
- * Lambda handler.
+ * Lambda handler to process a batch of SQS messages
  */
 export async function handler(event: SQSEvent, context: Context): Promise<SQSBatchResponse> {
-  try {
-    console.log(`Executing ${context.functionName} version: ${commitHash}`);
+  console.log(`Executing ${context.functionName} version: ${commitHash || 'development'}`);
 
-    // Process incoming message(s)
-    const promises = event.Records.map((record) => processRecord(record));
-    await Promise.all(promises);
+  // Process incoming message(s)
+  // and note any failures
+  const failuredIds: string[] = [];
+  const records = event.Records.map(async (record) => {
+    try {
+      await processMessage(record.body);
+    } catch (err) {
+      failuredIds.push(record.messageId);
+      console.error(`Message error: ${err} [${record.messageId}]`);
+    }
+  });
+  await Promise.all(records);
 
-    // Return any item failures for retry
-    return {
-      batchItemFailures: failedMessageIds.map((messageId) => ({ itemIdentifier: messageId }))
-    };
-  } catch (err) {
-    // Report the error to Lambda for retry
-    console.log(`Slack messaging error: ${err}`);
-    throw err;
-  }
+  // Report on any failred items for retry
+  const result: SQSBatchResponse = {
+    batchItemFailures: failuredIds.map((id) => ({ itemIdentifier: id })),
+  };
+  return result;
 }
