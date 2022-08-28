4deb0e03ec41d6653a5e3181cd7701688d50b2a3
David Carboni <david@carboni.io>
2022-08-28 15:22:15 +0100

    Added webApp pattern

--- infrastructure/src/scloud/apigateway.ts
+++ infrastructure/src/scloud/apigateway.ts
@@ -6,8 +6,104 @@ import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { DnsValidatedCertificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { ApiGateway } from 'aws-cdk-lib/aws-route53-targets';
+import { ApiGateway, CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import { RestApiOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { AllowedMethods, Distribution, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { HttpsRedirect } from 'aws-cdk-lib/aws-route53-patterns';
+import * as _ from 'lodash';
 import { containerFunction, zipFunction } from './lambdaFunction';
+
+function output(
+  construct: Construct,
+  type: string,
+  name: string,
+  value: string,
+) {
+  const outputName = `${_.capitalize(name)}${type}`;
+  new CfnOutput(construct, outputName, { value });
+}
+
+export function webApp(
+  construct: Construct,
+  name: string,
+  zone: route53.IHostedZone,
+  environment?: { [key: string]: string; },
+  memory: number = 2048,
+): { lambda: Function, api: LambdaRestApi, bucket: Bucket, distribution: Distribution; } {
+  const domainName = `${zone.zoneName}`;
+
+  // Web app handler
+  const lambda = zipFunction(construct, name, environment, memory);
+
+  // Static content
+  const bucket = new Bucket(construct, `${name}Static`, {
+    removalPolicy: RemovalPolicy.DESTROY,
+    autoDeleteObjects: true,
+    publicReadAccess: true,
+  });
+  output(construct, 'Bucket', name, bucket.bucketName);
+
+  const certificate = new DnsValidatedCertificate(construct, `${name}Certificate`, {
+    domainName,
+    subjectAlternativeNames: [`www.${domainName}`],
+    hostedZone: zone,
+    region: 'us-east-1',
+  });
+
+  const api = new apigateway.LambdaRestApi(construct, `${name}ApiGateway`, {
+    handler: lambda,
+    proxy: true,
+    binaryMediaTypes: ['multipart/form-data'],
+    domainName: {
+      domainName,
+      certificate,
+    },
+  });
+
+  const staticBehavior = {
+    origin: new S3Origin(bucket),
+    allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+    viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    compress: true,
+  };
+
+  const distribution = new Distribution(construct, `${name}Distribution`, {
+    domainNames: [domainName],
+    comment: domainName,
+    defaultBehavior: {
+      origin: new RestApiOrigin(api),
+      allowedMethods: AllowedMethods.ALLOW_ALL,
+      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      compress: true,
+    },
+    additionalBehaviors: {
+      '/public/': staticBehavior,
+      // inxex.html direct from s3 for latency on / route? // '/': staticBehaviour'
+    },
+    certificate,
+  });
+  output(construct, 'DistributionId', name, distribution.distributionId);
+
+  new route53.ARecord(construct, `${name}ARecord`, {
+    recordName: domainName,
+    target: route53.RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    zone,
+  });
+
+  // Redirect www -> zone root
+  new HttpsRedirect(construct, `${name}WwwRedirect`, {
+    recordNames: [`www.${domainName}`],
+    targetDomain: domainName,
+    certificate,
+    zone,
+  });
+
+  return {
+    lambda, api, bucket, distribution,
+  };
+}
 
 /**
  * An API gateway backed by a Lambda function.
@@ -17,7 +113,7 @@ import { containerFunction, zipFunction } from './lambdaFunction';
  * @param name The name for this gateway and associated resources
  * @param zone DNS zone to use for this API
  * @param environment Environment variables for the backing Lambda function
- * @param domainName Domain name to use for this API. Defaults to `api.${zone.zoneName}`
+ * @param apiDomainName Domain name to use for this API. Defaults to `api.${zone.zoneName}`
  * @returns
  */
 export function apiGateway(
@@ -26,10 +122,11 @@ export function apiGateway(
   zone: route53.IHostedZone,
   environment?: { [key: string]: string; },
   apiDomainName?: string,
+  memory: number = 128,
 ): { lambda: Function, api: LambdaRestApi; } {
   const domainName = apiDomainName || `api.${zone.zoneName}`;
 
-  const lambda = zipFunction(construct, name, environment, 2048, 100);
+  const lambda = zipFunction(construct, name, environment, memory);
 
   const api = new apigateway.LambdaRestApi(construct, `${name}ApiGateway`, {
     handler: lambda,
