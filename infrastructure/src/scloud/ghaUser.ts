43d1540e924334ac5dd13799beb1ae66c18b2048
David Carboni <david@carboni.io>
2022-08-31 14:24:40 +0100

    Reworked the Github Actions user

--- infrastructure/src/scloud/ghaUser.ts
+++ infrastructure/src/scloud/ghaUser.ts
@@ -1,149 +1,43 @@
-import { IRepository } from 'aws-cdk-lib/aws-ecr';
 import {
   CfnAccessKey, Effect, Policy, PolicyStatement, User,
 } from 'aws-cdk-lib/aws-iam';
+import { CfnOutput, Stack } from 'aws-cdk-lib';
+import { IRepository } from 'aws-cdk-lib/aws-ecr';
 import { IFunction } from 'aws-cdk-lib/aws-lambda';
 import { IFargateService } from 'aws-cdk-lib/aws-ecs';
-import { CfnOutput, Stack } from 'aws-cdk-lib';
 import { IBucket } from 'aws-cdk-lib/aws-s3';
 import { IDistribution } from 'aws-cdk-lib/aws-cloudfront';
+
+export const ghaResources = {
+  repositories: <IRepository[]>[],
+  buckets: <IBucket[]>[],
+  lambdas: <IFunction[]>[],
+  services: <IFargateService[]>[],
+  distributions: <IDistribution[]>[],
+};
+
+let user: User;
+
+function addPolicy(stack: Stack, name: string, resources: string[], actions: string[]) {
+  if (resources.length > 0) {
+    const statement = new PolicyStatement({
+      actions,
+      resources,
+    });
+    const policy = new Policy(stack, `gha${name}`, {
+      policyName: name,
+      statements: [statement],
+    });
+    user.attachInlinePolicy(policy);
+  }
+}
 
 /**
  * A user for Gihud Actions CI/CD.
  */
-export default function ghaUser(
-  stack: Stack,
-  ecrRepositories: IRepository[],
-  buckets?: IBucket[],
-  lambdas?: IFunction[],
-  // edgeLambdas?: cloudfront.experimental.EdgeFunction[],
-  services?: IFargateService[],
-  distributions?: IDistribution[],
-): CfnAccessKey | undefined {
-  const statements: PolicyStatement[] = [];
-
-  // ECR login
-  if (ecrRepositories.length > 0) {
-    statements.push(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: [
-        'ecr:GetAuthorizationToken',
-      ],
-      resources: [
-        '*',
-      ],
-    }));
-  }
-
-  // ECR repositories
-  const repositoryArns = ecrRepositories
-    .filter((repository) => repository !== undefined)
-    .map((repository) => repository.repositoryArn);
-  if (repositoryArns.length > 0) {
-    statements.push(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: [
-        'ecr:GetDownloadUrlForLayer',
-        'ecr:BatchGetImage',
-        'ecr:BatchDeleteImage',
-        'ecr:CompleteLayerUpload',
-        'ecr:UploadLayerPart',
-        'ecr:InitiateLayerUpload',
-        'ecr:BatchCheckLayerAvailability',
-        'ecr:PutImage',
-        'ecr:ListImages',
-      ],
-      resources: repositoryArns,
-    }));
-  }
-
-  // Buckets
-  if (buckets !== undefined && buckets.length > 0) {
-    // s3 needs both arn and arn/* to have access to the bucket and to bucket objects
-    const bucketArns = buckets
-      .filter((bucket) => bucket !== undefined)
-      .map((bucket) => bucket.bucketArn);
-    bucketArns.forEach((arn) => {
-      bucketArns.push(`${arn}/*`);
-    });
-    if (bucketArns.length > 0) {
-      statements.push(new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: [
-          's3:ListBucket',
-          's3:PutObject',
-          's3:DeleteObject',
-        ],
-        resources: bucketArns,
-      }));
-    }
-  }
-
-  // Lambda functions
-  let lambdaArns: string[] = [];
-  if (lambdas !== undefined && lambdas.length > 0) {
-    const functionArns = lambdas
-      .filter((lambda) => lambda !== undefined)
-      .map((lambda) => lambda.functionArn);
-    lambdaArns = lambdaArns.concat(functionArns);
-  }
-  // if (edgeLambdas) {
-  //   const edgeArns = edgeLambdas
-  //     .filter((lambda) => lambda !== undefined)
-  //     .map((lambda) => lambda.functionArn);
-  //   lambdaArns = lambdaArns.concat(edgeArns);
-  // }
-  if (lambdaArns.length > 0) {
-    statements.push(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: [
-        'lambda:UpdateFunctionCode',
-        'lambda:PublishVersion',
-      ],
-      resources: lambdaArns,
-    }));
-  }
-
-  // Fargate services
-  if (services !== undefined && services.length > 0) {
-    const serviceArns = services
-      .filter((service) => service !== undefined)
-      .map((service) => service.serviceArn);
-    if (serviceArns.length > 0) {
-      statements.push(new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: [
-          'ecs:UpdateService',
-        ],
-        resources: serviceArns,
-      }));
-    }
-  }
-
-  // Cloudfront distribution invalidation
-  if (distributions !== undefined && distributions.length > 0) {
-    const distributionArns = distributions
-      .filter((distribution) => distribution !== undefined)
-      // Not sure where to 'properly' get a distribution ARN from?
-      .map((distribution) => `arn:aws:cloudfront::${stack.account}:distribution/${distribution.distributionId}`);
-    statements.push(new PolicyStatement({
-      effect: Effect.ALLOW,
-      actions: [
-        'cloudfront:CreateInvalidation',
-      ],
-      resources: distributionArns,
-    }));
-  }
-
-  // A policy that includes these statments
-  const ghaPolicy = new Policy(stack, 'ghaUserPolicy', {
-    policyName: 'ghaUserPolicy',
-    statements,
-  });
-
+export function ghaUser(stack: Stack): CfnAccessKey | undefined {
   // A user with the policy attached
-  const user = new User(stack, 'ghaUser', { userName: `gha-${stack.stackName.toLowerCase()}` });
-  user.attachInlinePolicy(ghaPolicy);
+  user = new User(stack, 'ghaUser', { userName: `gha-${stack.stackName.toLowerCase()}` });
 
   // Credentials
   let accessKey: CfnAccessKey | undefined;
@@ -152,10 +46,63 @@ export default function ghaUser(
       userName: user.userName,
     });
 
-    // Access key details for use in setting GHA secrets
+    // Access key details for GHA secrets
     new CfnOutput(stack, 'awsAccessKeyId', { value: accessKey.ref });
     new CfnOutput(stack, 'awsSecretAccessKey', { value: accessKey.attrSecretAccessKey });
   }
+
+  // ECR repositories - push/pull images
+  const repositoryArns = ghaResources.repositories
+    .filter((repository) => repository)
+    .map((repository) => repository.repositoryArn);
+  addPolicy(stack, 'ecrLogin', ['*'], ['ecr:GetAuthorizationToken']);
+  addPolicy(stack, 'ecrRepositories', repositoryArns, [
+    'ecr:GetDownloadUrlForLayer',
+    'ecr:BatchGetImage',
+    'ecr:BatchDeleteImage',
+    'ecr:CompleteLayerUpload',
+    'ecr:UploadLayerPart',
+    'ecr:InitiateLayerUpload',
+    'ecr:BatchCheckLayerAvailability',
+    'ecr:PutImage',
+    'ecr:ListImages',
+  ]);
+
+  // Buckets - upload/sync
+  const bucketArns = ghaResources.buckets
+    .filter((bucket) => bucket)
+    .map((bucket) => bucket.bucketArn);
+  addPolicy(stack, 'buckets', bucketArns, [
+    's3:ListBucket',
+    's3:PutObject',
+    's3:DeleteObject',
+  ]);
+
+  // Lambdas - update update with a new zip/container build
+  const lambdaArns = ghaResources.lambdas
+    .filter((lambda) => lambda)
+    .map((lambda) => lambda.functionArn);
+  addPolicy(stack, 'lambdas', lambdaArns, [
+    'lambda:UpdateFunctionCode',
+    // 'lambda:PublishVersion',
+  ]);
+
+  // Fargate services - update with a new container build
+  const serviceArns = ghaResources.services
+    .filter((service) => service)
+    .map((service) => service.serviceArn);
+  addPolicy(stack, 'fargateServices', serviceArns, [
+    'ecs:UpdateService',
+  ]);
+
+  // Cloudfront distribution - cache invalidation
+  const distributionArns = ghaResources.distributions
+    .filter((distribution) => distribution !== undefined)
+    // Not sure where to 'properly' get a distribution ARN from?
+    .map((distribution) => `arn:aws:cloudfront::${stack.account}:distribution/${distribution.distributionId}`);
+  addPolicy(stack, 'distributions', distributionArns, [
+    'cloudfront:CreateInvalidation',
+  ]);
 
   return accessKey;
 }
