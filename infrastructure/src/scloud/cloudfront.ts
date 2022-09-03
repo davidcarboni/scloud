7eb0774be1e45930b4eeb5c1197c8c44488ceb8d
David Carboni <david@carboni.io>
2022-09-03 14:50:32 +0100

    Move webApp from apigateway.ts to cloudfront.ts

--- infrastructure/src/scloud/cloudfront.ts
+++ infrastructure/src/scloud/cloudfront.ts
@@ -6,14 +6,126 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as route53patterns from 'aws-cdk-lib/aws-route53-patterns';
 import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
+import { DnsValidatedCertificate, ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
-import {
-  AllowedMethods, Distribution, ViewerProtocolPolicy, // LambdaEdgeEventType,
-} from 'aws-cdk-lib/aws-cloudfront';
-// import { IFunction } from 'aws-cdk-lib/aws-lambda';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { ICertificate } from 'aws-cdk-lib/aws-certificatemanager';
-// import * as lambda from './lambdaFunction';
+import { RestApiOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import {
+  AllowedMethods, CachePolicy, Distribution, OriginRequestCookieBehavior,
+  OriginRequestHeaderBehavior, OriginRequestPolicy, OriginRequestQueryStringBehavior,
+  ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Function } from 'aws-cdk-lib/aws-lambda';
+import _ from 'lodash';
+import { zipFunction } from './lambdaFunction';
+
+function output(
+  construct: Construct,
+  type: string,
+  name: string,
+  value: string,
+) {
+  const outputName = `${_.lowerFirst(name)}${_.capitalize(type)}`;
+  new CfnOutput(construct, outputName, { value });
+}
+
+export function redirectWww(
+  construct: Construct,
+  name: string,
+  zone: route53.IHostedZone,
+  certificate?: ICertificate,
+) {
+  new route53patterns.HttpsRedirect(construct, `${name}WwwRedirect`, {
+    targetDomain: zone.zoneName,
+    recordNames: [`www.${zone.zoneName}`],
+    zone,
+    certificate: certificate || new acm.DnsValidatedCertificate(construct, `${name}WwwCertificate`, {
+      domainName: `www.${zone.zoneName}`,
+      hostedZone: zone,
+      // this is required for Cloudfront certificates:
+      // https://docs.aws.amazon.com/cdk/api/v1/docs/aws-cloudfront-readme.html
+      region: 'us-east-1',
+    }),
+  });
+}
+
+export function webApp(
+  construct: Construct,
+  name: string,
+  zone: route53.IHostedZone,
+  environment?: { [key: string]: string; },
+  memory: number = 2048,
+): { lambda: Function, api: LambdaRestApi, bucket: Bucket, distribution: Distribution; } {
+  const domainName = `${zone.zoneName}`;
+
+  // Web app handler
+  const lambda = zipFunction(construct, name, environment, memory);
+
+  // Static content
+  const bucket = new Bucket(construct, `${name}Static`, {
+    removalPolicy: RemovalPolicy.DESTROY,
+    autoDeleteObjects: true,
+    publicReadAccess: true,
+  });
+  output(construct, 'StaticBucket', name, bucket.bucketName);
+
+  const api = new LambdaRestApi(construct, `${name}ApiGateway`, {
+    handler: lambda,
+    proxy: true,
+    binaryMediaTypes: ['multipart/form-data'],
+  });
+
+  const staticBehavior = {
+    origin: new S3Origin(bucket),
+    allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+    viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    compress: true,
+  };
+
+  const certificate = new DnsValidatedCertificate(construct, `${name}Certificate`, {
+    domainName,
+    subjectAlternativeNames: [`www.${domainName}`],
+    hostedZone: zone,
+    region: 'us-east-1',
+  });
+
+  const distribution = new Distribution(construct, `${name}Distribution`, {
+    domainNames: [domainName],
+    comment: domainName,
+    defaultBehavior: {
+      origin: new RestApiOrigin(api),
+      allowedMethods: AllowedMethods.ALLOW_ALL,
+      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      compress: true,
+      cachePolicy: CachePolicy.CACHING_DISABLED,
+      // https://stackoverflow.com/questions/71367982/cloudfront-gives-403-when-origin-request-policy-include-all-headers-querystri
+      // What we would actually want (it seems) it to not pass the host header
+      originRequestPolicy: new OriginRequestPolicy(construct, `${name}OriginRequestPolicy`, {
+        headerBehavior: OriginRequestHeaderBehavior.allowList('user-agent'),
+        cookieBehavior: OriginRequestCookieBehavior.all(),
+        queryStringBehavior: OriginRequestQueryStringBehavior.all(),
+      }), // OriginRequestPolicy.ALL_VIEWER,
+    },
+    additionalBehaviors: {
+      '/public/*': staticBehavior,
+      // inxex.html direct from s3 for latency on / route? // '/': staticBehaviour'
+    },
+    certificate,
+  });
+  output(construct, 'DistributionId', name, distribution.distributionId);
+
+  new route53.ARecord(construct, `${name}ARecord`, {
+    recordName: domainName,
+    target: route53.RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    zone,
+  });
+  redirectWww(construct, name, zone, certificate);
+
+  return {
+    lambda, api, bucket, distribution,
+  };
+}
 
 /**
  * A Cloudfront distribution backed by an s3 bucket.
@@ -111,25 +223,3 @@ export function redirect(
     }),
   });
 }
-
-export function redirectWww(
-  construct: Construct,
-  name: string,
-  zone: route53.IHostedZone,
-  certificate?: ICertificate,
-) {
-  new route53patterns.HttpsRedirect(construct, `${name}WwwRedirect`, {
-    targetDomain: zone.zoneName,
-    recordNames: [`www.${zone.zoneName}`],
-    zone,
-    certificate: certificate || new acm.DnsValidatedCertificate(construct, `${name}WwwCertificate`, {
-      domainName: `www.${zone.zoneName}`,
-      hostedZone: zone,
-      // this is required for Cloudfront certificates:
-      // https://docs.aws.amazon.com/cdk/api/v1/docs/aws-cloudfront-readme.html
-      region: 'us-east-1',
-    }),
-  });
-}
-
-// Update edge lambda: https://stackoverflow.com/questions/50967018/awscli-lambda-function-update-trigger
