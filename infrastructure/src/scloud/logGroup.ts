fc3d7546c105ea3824c9251bd9c16b44ea7b13e7
David Carboni <david@carboni.io>
2022-09-03 14:49:54 +0100

    Currently unused but might be handy at some point

---
+++ infrastructure/src/scloud/logGroup.ts
@@ -0,0 +1,24 @@
+import { RemovalPolicy, Stack } from 'aws-cdk-lib';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
+
+/**
+ * Builds a LogGroup.
+ * @param stack Parent CDK stack
+ * @param category Used to build up the logGroupName: /stackName/categpry/name
+ * @param name Name (and base for an ID) for the logGroup
+ * @param retention Defaults to RetentionDays.ONE_MONTH
+ */
+export default function logGroup(
+  stack: Stack,
+  category: string,
+  name: string,
+  retention: RetentionDays = RetentionDays.THREE_MONTHS,
+) : LogGroup {
+  return new LogGroup(stack, `${name}LogGroup`, {
+  // Ensure the log group is deleted when the stack is deleted
+  // and that logs aren't retained indefinitely
+    logGroupName: `/${stack.stackName}/ecs/${name}`,
+    removalPolicy: RemovalPolicy.DESTROY,
+    retention,
+  });
+}
