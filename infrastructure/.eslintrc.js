93d8bb7dc97f40a05cddb70775a9e1cc5451ed9d
David <david.carboni@policyinpractice.co.uk>
2022-11-11 15:45:56 +0000

    line length 150

--- infrastructure/.eslintrc.js
+++ infrastructure/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    mocha: true,
   },
   extends: [
     'airbnb-base',
@@ -25,14 +26,47 @@ module.exports = {
         tsx: 'never',
       },
     ],
+    quotes: [2, 'single', 'avoid-escape'],
+    'max-len': [
+      'error',
+      {
+        code: 150,
+        tabWidth: 2,
+        ignoreComments: true, // comments: 80
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
     'no-new': 'off', // Because CDK likes to 'new' things
     'no-console': 'off', // Because we want to print some things out about the stack.
   },
   settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.js'],
+    },
     'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
+  overrides: [
+    {
+      // We lint unit tests differently.
+      files: ['*.spec.ts'],
+      rules: {
+
+        // Avoid mocha/chai "Expected an assignment or function call and instead saw an expression"
+        'no-unused-expressions': 'off',
+
+        // Avoid "* should be listed in the project's dependencies, not devDependencies"
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+  ignorePatterns: ['node_modules', 'dist'],
 };
