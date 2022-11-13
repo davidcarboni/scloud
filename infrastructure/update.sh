31948bf3b310596f8e742ddf58f40f1061557c54
David Carboni <david@carboni.io>
2022-11-13 15:52:16 +0000

    Improved update script

--- infrastructure/update.sh
+++ infrastructure/update.sh
@@ -17,20 +17,29 @@ fi
 echo "Starting infrastructure build: $(date)"
 npm run lint
 
-# Full deploy
-cdk deploy --all --outputs-file ./secrets/cdk-outputs.json
+# Show differences
+cdk diff
+
+read -p "Do you want to proceed? (y/n) " yn
+case $yn in
+	y ) echo Deploying...;;
+	* ) echo Exit;
+		exit 0;;
+esac
+
+cdk deploy --outputs-file ./secrets/cdk-outputs.json
 
 # Update secrets
 echo "Setting Github secrets"
 npm run secrets
 
 # Build an environment file for Docker Compose to run locally
-echo "Writing Docker Compose environment file"
-npm run compose
-git commit -m "Infrastructure build" ../docker-compose.env
+# echo "Writing Docker Compose environment file"
+# npm run compose
+# git commit -m "Infrastructure build" ../docker-compose.env
 
 # Store the names of the ECR repos
 # Because they're different every time the inferastructure is rebuilt and we'll need them to run delete.sh
-npm run ecr-repos
+# npm run ecr-repos
 
 echo "End: $(date)"
