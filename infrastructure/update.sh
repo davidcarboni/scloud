808c77856c199f08ba5c5ad3ac77063b74dbe206
David Carboni <david@carboni.io>
2022-12-30 22:52:25 +0000

    Skip approval on the basis we've already done a diff to avoid a repeat y/n prompt

--- infrastructure/update.sh
+++ infrastructure/update.sh
@@ -30,7 +30,8 @@ case $yn in
 		exit 0;;
 esac
 
-cdk deploy --outputs-file secrets/cdk-outputs.json
+# Skip approval on the basis we've already done a diff above so this creates a repeat y/n prompt:
+cdk deploy --require-approval never --outputs-file ./secrets/cdk-outputs.json
 
 # Update secrets
 echo "Setting Github secrets"
