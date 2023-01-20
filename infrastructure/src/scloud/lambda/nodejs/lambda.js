40309ad7dedf4141de40458fc232f41dd5bf3e17
David <david.carboni@policyinpractice.co.uk>
2023-01-20 12:56:44 +0000

    Placeholder code

--- infrastructure/src/scloud/lambda/nodejs/lambda.js
+++ infrastructure/src/scloud/lambda/nodejs/lambda.js
@@ -1,9 +1,7 @@
 exports.handler = async (event) => {
   console.log('Placeholder function code.');
-  const input = JSON.stringify(event);
-  console.log(input);
   return {
     status: 200,
-    body: input,
+    body: 'Placeholder function code.',
   };
 };
---
+++ infrastructure/src/scloud/lambda/python/lambda.py
@@ -0,0 +1,7 @@
+
+def handler():
+  print("Placeholder function code.")
+  return {
+    "status": 200,
+    "body": "Placeholder function code.",
+  }
