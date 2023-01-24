df2b7b224d06c01f1e98bbf969819bfa68e0a8e1
David Carboni <david@carboni.io>
2023-01-24 20:53:42 +0000

    Adding a Cloudfront API gateway and a readable description for API gateways

--- infrastructure/src/scloud/apigateway.ts
+++ infrastructure/src/scloud/apigateway.ts
@@ -6,8 +6,85 @@ import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { DnsValidatedCertificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { ApiGateway } from 'aws-cdk-lib/aws-route53-targets';
+import { ApiGateway, CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import {
+  AllowedMethods, CachePolicy, Distribution, OriginRequestCookieBehavior,
+  OriginRequestHeaderBehavior, OriginRequestPolicy, OriginRequestQueryStringBehavior, ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { RestApiOrigin, S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { containerFunction, zipFunctionTypescript } from './lambdaFunction';
+
+export function cloudfrontApiGateway(
+  construct: Construct,
+  name: string,
+  zone: route53.IHostedZone,
+  environment?: { [key: string]: string; },
+  domain?: string,
+  memory: number = 256,
+  routes: string[] = ['/'],
+): { lambda: Function, distribution: Distribution; } {
+  const domainName = domain || `${zone.zoneName}`;
+
+  const lambda = zipFunctionTypescript(construct, name, environment, { memorySize: memory });
+
+  // Cloudfromt distribution
+  const distribution = new Distribution(construct, `${name}Distribution`, {
+    domainNames: [domainName],
+    comment: domainName,
+    defaultBehavior: {
+      // Request bin: default is to deflect all requests that aren't known to the API - mostly scripts probing for Wordpress installations
+      origin: new S3Origin(new Bucket(construct, `${name}RequestBin`, {
+        removalPolicy: RemovalPolicy.DESTROY,
+        autoDeleteObjects: true,
+        publicReadAccess: true,
+      })),
+      allowedMethods: AllowedMethods.ALLOW_ALL,
+      viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      cachePolicy: CachePolicy.CACHING_OPTIMIZED,
+      originRequestPolicy: new OriginRequestPolicy(construct, `${name}BinOriginRequestPolicy`, {
+        headerBehavior: OriginRequestHeaderBehavior.allowList('user-agent', 'User-Agent', 'Referer', 'referer', 'Bearer', 'bearer'),
+        cookieBehavior: OriginRequestCookieBehavior.all(),
+        queryStringBehavior: OriginRequestQueryStringBehavior.all(),
+      }),
+    },
+    certificate: new DnsValidatedCertificate(construct, `${name}Certificate`, {
+      domainName,
+      hostedZone: zone,
+      region: 'us-east-1',
+    }),
+  });
+
+  // Handle API requests
+  const apiOrigin = new RestApiOrigin(new apigateway.LambdaRestApi(construct, `${name}Api`, {
+    handler: lambda,
+    proxy: true,
+    description: name,
+  }));
+  const apiOptions = {
+    allowedMethods: AllowedMethods.ALLOW_ALL,
+    viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    compress: true,
+    cachePolicy: CachePolicy.CACHING_DISABLED,
+    // https://stackoverflow.com/questions/71367982/cloudfront-gives-403-when-origin-request-policy-include-all-headers-querystri
+    // OriginRequestHeaderBehavior.all() gives an error so just cookie, user-agent, referer
+    originRequestPolicy: new OriginRequestPolicy(construct, `${name}OriginRequestPolicy`, {
+      headerBehavior: OriginRequestHeaderBehavior.allowList('user-agent', 'User-Agent', 'Referer', 'referer'),
+      cookieBehavior: OriginRequestCookieBehavior.all(),
+      queryStringBehavior: OriginRequestQueryStringBehavior.all(),
+    }),
+  };
+  routes.forEach((path) => distribution.addBehavior(path, apiOrigin, apiOptions));
+
+  new route53.ARecord(construct, `${name}ARecord`, {
+    recordName: domainName,
+    target: route53.RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    zone,
+  });
+
+  return { lambda, distribution };
+}
 
 /**
  * An API gateway backed by a Lambda function.
@@ -35,7 +112,7 @@ export function apiGateway(
   const api = new apigateway.LambdaRestApi(construct, `${name}ApiGateway`, {
     handler: lambda,
     proxy: true,
-    binaryMediaTypes: ['multipart/form-data'],
+    description: name,
     domainName: {
       domainName,
       certificate: new DnsValidatedCertificate(construct, `${name}Certificate`, {
@@ -74,7 +151,7 @@ export function apiGatewayContainer(
   zone: route53.IHostedZone,
   environment?: { [key: string]: string; },
   apiDomainName?: string,
-): { lambda: Function, repository: Repository, api: LambdaRestApi; } {
+): { lambda: Function, api: LambdaRestApi, repository: Repository; } {
   const domainName = apiDomainName || `api.${zone.zoneName}`;
 
   const { lambda, repository } = containerFunction(construct, initialPass, name, environment);
@@ -82,6 +159,7 @@ export function apiGatewayContainer(
   const api = new apigateway.LambdaRestApi(construct, `${name}ApiGateway`, {
     handler: lambda,
     proxy: true,
+    description: name,
     domainName: {
       domainName,
       certificate: new DnsValidatedCertificate(construct, `${name}Certificate`, {
@@ -99,5 +177,5 @@ export function apiGatewayContainer(
     comment: `A record for API gateway ${name} API gateway`,
   });
 
-  return { lambda, repository, api };
+  return { lambda, api, repository };
 }
