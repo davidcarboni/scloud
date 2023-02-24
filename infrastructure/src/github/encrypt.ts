cad0b93621b077ac57d0b2242f52ff11c2e6aa2d
David <david.carboni@policyinpractice.co.uk>
2023-02-24 12:05:04 +0000

    Update based o example code in the Github API docs

--- infrastructure/src/github/encrypt.ts
+++ infrastructure/src/github/encrypt.ts
@@ -1,14 +1,22 @@
-import * as libsodium from 'libsodium-wrappers';
+import * as sodium from 'libsodium-wrappers';
 
+/**
+ * Based on: https://docs.github.com/en/rest/actions/secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
+ * @param secretValue The secret value to be encrypted
+ * @param key The key to use for encryption (eg repository public key)
+ * @returns The encrypted value (a base-64 string)
+ */
 export default async function encrypt(secretValue: string, key: string): Promise<string> {
-  // Convert the message and key to Uint8Array's (Buffer implements that interface)
-  const messageBytes = Buffer.from(secretValue);
-  const keyBytes = Buffer.from(key, 'base64');
+  // Convert Secret & Base64 key to Uint8Array.
+  const secretBytes = sodium.from_string(secretValue);
+  const keyBytes = sodium.from_base64(key, sodium.base64_variants.ORIGINAL);
 
   // Encrypt using LibSodium.
-  await libsodium.ready;
-  const encryptedBytes = libsodium.crypto_box_seal(messageBytes, keyBytes);
-  const encryptedB64 = Buffer.from(encryptedBytes).toString('base64');
+  await sodium.ready;
+  const encryptedBytes = sodium.crypto_box_seal(secretBytes, keyBytes);
+
+  // Convert encrypted Uint8Array to Base64
+  const encryptedB64 = sodium.to_base64(encryptedBytes, sodium.base64_variants.ORIGINAL);
 
   return encryptedB64;
 }
