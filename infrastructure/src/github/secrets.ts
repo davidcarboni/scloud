f2f33fcdf52fc991243cee5b3a474ec84f13384c
David <david.carboni@policyinpractice.co.uk>
2023-03-15 19:28:32 +0000

    Reworking type information to harmonise secrets and variables

--- infrastructure/src/github/secrets.ts
+++ infrastructure/src/github/secrets.ts
@@ -12,8 +12,15 @@ const repo = process.env.REPO || '';
 if (!owner) throw new Error('No repo owner: please set an environment variable for either USERNAME or OWNER');
 if (!repo) throw new Error('No repo: please set an environment variable for REPO');
 
+// Type-checking interfaces that represent collections of variable/secret key-value pairs
 interface KeyValues {
   [name: string]: string
+}
+interface KeyValuesCollection {
+  repo: KeyValues, environment: { [environment: string]: KeyValues; };
+}
+interface KeysCollection {
+  repo: string[], environment: { [environment: string]: string[]; };
 }
 
 function envVarCase(camelCaseName: string): string {
@@ -21,8 +28,8 @@ function envVarCase(camelCaseName: string): string {
   return snakeCase.toUpperCase();
 }
 
-async function readVariables(): Promise<{repoVariables:KeyValues, environmentVariables: {[environment:string]: KeyValues}}> {
-  const variables: {repoVariables:KeyValues, environmentVariables: {[environment:string]: KeyValues}} = { repoVariables: {}, environmentVariables: {} };
+async function readVariables(): Promise<KeyValuesCollection> {
+  const variables: KeyValuesCollection = { repo: {}, environment: {} };
 
   const cdkOutputs = 'secrets/cdk-outputs.json';
   // const awsConfig = '~/.aws/credentials';
@@ -36,12 +43,12 @@ async function readVariables(): Promise<{repoVariables:KeyValues, environmentVar
           const variableNames = JSON.parse(readFileSync(`secrets/${stackName}.ghaVariables.json`, 'utf-8')) as string[];
 
           const environment = stacks[stackName];
-          if (environment) { variables.environmentVariables[environment] = {}; }
+          if (environment) { variables.environment[environment] = {}; }
           variableNames.forEach((variableName) => {
             if (environment) {
-              variables.environmentVariables[environment][envVarCase(variableName)] = outputs[stackName][variableName];
+              variables.environment[environment][envVarCase(variableName)] = outputs[stackName][variableName];
             } else {
-              variables.repoVariables[envVarCase(variableName)] = outputs[stackName][variableName];
+              variables.repo[envVarCase(variableName)] = outputs[stackName][variableName];
             }
           });
         } else {
@@ -56,8 +63,8 @@ async function readVariables(): Promise<{repoVariables:KeyValues, environmentVar
   throw new Error(`Couldn't find file ${cdkOutputs}`);
 }
 
-async function readSecrets(): Promise<{repoSecrets:KeyValues, environmentSecrets: {[environment:string]: KeyValues}}> {
-  const secrets: {repoSecrets:KeyValues, environmentSecrets: {[environment:string]: KeyValues}} = { repoSecrets: {}, environmentSecrets: {} };
+async function readSecrets(): Promise<KeyValuesCollection> {
+  const secrets: KeyValuesCollection = { repo: {}, environment: {} };
 
   const cdkOutputs = 'secrets/cdk-outputs.json';
   // const awsConfig = '~/.aws/credentials';
@@ -71,12 +78,12 @@ async function readSecrets(): Promise<{repoSecrets:KeyValues, environmentSecrets
           const secretNames = JSON.parse(readFileSync(`secrets/${stackName}.ghaSecrets.json`, 'utf-8')) as string[];
 
           const environment = stacks[stackName];
-          if (environment) { secrets.environmentSecrets[environment] = {}; }
+          if (environment) { secrets.environment[environment] = {}; }
           secretNames.forEach((secretName) => {
             if (environment) {
-              secrets.environmentSecrets[environment][envVarCase(secretName)] = outputs[stackName][secretName];
+              secrets.environment[environment][envVarCase(secretName)] = outputs[stackName][secretName];
             } else {
-              secrets.repoSecrets[envVarCase(secretName)] = outputs[stackName][secretName];
+              secrets.repo[envVarCase(secretName)] = outputs[stackName][secretName];
             }
           });
         } else {
@@ -91,142 +98,155 @@ async function readSecrets(): Promise<{repoSecrets:KeyValues, environmentSecrets
   throw new Error(`Couldn't find file ${cdkOutputs}`);
 }
 
+/**
+ * Handles synching variables with Github
+ */
+async function processVariables(): Promise<KeyValuesCollection> {
+  const currentVariableNames: KeysCollection = {
+    repo: [],
+    environment: {},
+  };
+
+  // List the current variables
+  currentVariableNames.repo = await gh.listRepoVariables(owner, repo);
+  console.log(`${owner}/${repo} has ${currentVariableNames.repo.length} variables: ${currentVariableNames.repo}`);
+
+  await Promise.all(Object.keys(stacks).map(async (stackName) => {
+    const environment = stacks[stackName];
+    const envVars = await gh.listEnvironmentVariables(owner, repo, environment);
+    currentVariableNames.environment[environment] = envVars;
+    console.log(`${owner}/${repo}/${environment} has ${envVars.length} variables: ${envVars}`);
+  }));
+
+  // Parse the input json (read the variables that we intend to set on the repo and environments)
+  const newVariables = await readVariables();
+
+  // Confirm the list of variables to be set
+  console.log(`Variables to be set: ${JSON.stringify(newVariables, null, 2)}`);
+
+  const leftoverVariableNames = {
+    repo: <string[]>[],
+    environment: <{ [environment: string]: string[]; }>{},
+  };
+
+  leftoverVariableNames.repo = currentVariableNames.repo.filter((item) => !Object.keys(newVariables.repo).includes(item));
+  Object.keys(currentVariableNames.environment).forEach((environment) => {
+    leftoverVariableNames.environment[environment] = currentVariableNames.environment[environment].filter(
+      (item) => !Object.keys(newVariables.environment[environment]).includes(item),
+    );
+  });
+
+  // List out any leftover variables
+  if (leftoverVariableNames.repo.length > 0) {
+    console.log(` * NB: Some repo variables were not included in the CloudFormation outputs (${leftoverVariableNames.repo.length}):`);
+    leftoverVariableNames.repo.forEach((variableName) => console.log(` - ${variableName}`));
+  }
+  Object.keys(leftoverVariableNames.environment).forEach((environment) => {
+    if (leftoverVariableNames.environment[environment].length > 0) {
+      console.log(` * NB: Some environment variables in ${environment} were not included in the CloudFormation outputs (${leftoverVariableNames.environment[environment].length}):`);
+      const environmentVariables = leftoverVariableNames.environment[environment];
+      environmentVariables.forEach((variableName) => console.log(` - ${variableName}`));
+    }
+  });
+
+  // Delete any leftover variables
+  await Promise.all(leftoverVariableNames.repo.map(async (variableName) => gh.deleteRepoVariable(variableName, owner, repo)));
+  await Promise.all(Object.keys(leftoverVariableNames.environment).map(async (environment) => {
+    const environmentVariables = leftoverVariableNames.environment[environment];
+    await Promise.all(environmentVariables.map(async (secretName) => gh.deleteEnvironmentVariable(secretName, owner, repo, environment)));
+  }));
+
+  // Set variables
+  await Promise.all(Object.keys(newVariables.repo).map(async (variableName) => gh.setRepoVariable(variableName, newVariables.repo[variableName], owner, repo)));
+  await Promise.all(Object.keys(newVariables.environment).map(async (environment) => {
+    const environmentVariables = newVariables.environment[environment];
+    Object.keys(environmentVariables).map(async (variableName) => gh.setEnvironmentVariable(variableName, environmentVariables[variableName], owner, repo, environment));
+  }));
+
+  return newVariables;
+}
+
+/**
+ * Handles synching secrets with Github
+ */
+async function processSecrets(): Promise<KeyValuesCollection> {
+  const currentSecretNames: KeysCollection = {
+    repo: [],
+    environment: {},
+  };
+
+  // List the current secrets
+  currentSecretNames.repo = await gh.listRepoSecrets(owner, repo);
+  console.log(`${owner}/${repo} has ${currentSecretNames.repo.length} secrets: ${currentSecretNames.repo}`);
+
+  await Promise.all(Object.keys(stacks).map(async (stackName) => {
+    const environment = stacks[stackName];
+    const envSecrets = await gh.listEnvironmentSecrets(owner, repo, environment);
+    currentSecretNames.environment[environment] = envSecrets;
+    console.log(`${owner}/${repo}/${environment} has ${envSecrets.length} secrets: ${envSecrets}`);
+  }));
+
+  // Parse the input json (read the secrets that we intend to set on the repo and environments)
+  const newSecrets = await readSecrets();
+
+  const newSecretNames: KeysCollection = { repo: [], environment: {} };
+  newSecretNames.repo = Object.keys(newSecrets.repo);
+  Object.keys(newSecrets.environment).forEach((environment) => {
+    newSecretNames.environment[environment] = Object.keys(newSecrets.environment[environment]);
+  });
+  console.log(`Secrets (names) to be set: ${JSON.stringify(newSecretNames, null, 2)}`);
+
+  // Calculate any leftover secrets or variables (existing in GHA but not in our infrastructure code)
+  const leftoverSecretNames = {
+    repo: <string[]>[],
+    environment: <{ [environment: string]: string[]; }>{},
+  };
+
+  leftoverSecretNames.repo = currentSecretNames.repo.filter((item) => !Object.keys(newSecrets.repo).includes(item));
+  Object.keys(currentSecretNames.environment).forEach((environment) => {
+    leftoverSecretNames.environment[environment] = currentSecretNames.environment[environment].filter(
+      (item) => !Object.keys(newSecrets.environment[environment]).includes(item),
+    );
+  });
+
+  // List out any leftover secrets
+  if (leftoverSecretNames.repo.length > 0) {
+    console.log(` * NB: Some repo secrets were not included in the CloudFormation outputs (${leftoverSecretNames.repo.length}):`);
+    leftoverSecretNames.repo.forEach((secretName) => console.log(` - ${secretName}`));
+  }
+  Object.keys(leftoverSecretNames.environment).forEach((environment) => {
+    if (leftoverSecretNames.environment[environment].length > 0) {
+      console.log(` * NB: Some environment secrets in ${environment} were not included in the CloudFormation outputs (${leftoverSecretNames.environment[environment].length}):`);
+      const environmentSecrets = leftoverSecretNames.environment[environment];
+      environmentSecrets.forEach((secretName) => console.log(` - ${secretName}`));
+    }
+  });
+
+  // Delete any leftover secrets
+  await Promise.all(leftoverSecretNames.repo.map(async (secretName) => gh.deleteRepoSecret(secretName, owner, repo)));
+  await Promise.all(Object.keys(leftoverSecretNames.environment).map(async (environment) => {
+    const environmentSecrets = leftoverSecretNames.environment[environment];
+    await Promise.all(environmentSecrets.map(async (secretName) => gh.deleteEnvironmentSecret(secretName, owner, repo, environment)));
+  }));
+
+  // Set secrets
+  await Promise.all(Object.keys(newSecrets.repo).map(async (secretName) => gh.setRepoSecret(secretName, newSecrets.repo[secretName], owner, repo)));
+  await Promise.all(Object.keys(newSecrets.environment).map(async (environment) => {
+    const environmentSecrets = newSecrets.environment[environment];
+    Object.keys(environmentSecrets).map(async (secretName) => gh.setEnvironmentSecret(secretName, environmentSecrets[secretName], owner, repo, environment));
+  }));
+
+  return newSecrets;
+}
+
 (async () => {
   console.log(`Updating variables and secrets on ${owner}/${repo}`);
   try {
     // Cache the repo public key
     await gh.getRepoPublicKey(owner, repo);
 
-    const currentSecretNames: {
-      repoSecrets:string[],
-      environmentSecrets: {[environment:string]:string[]},
-    } = {
-      repoSecrets: [],
-      environmentSecrets: {},
-    };
-
-    const currentVariableNames: {
-      repoVariables:string[],
-      environmentVariables: {[environment:string]:string[]}
-    } = {
-      repoVariables: [],
-      environmentVariables: {},
-    };
-
-    // List the current secrets
-    currentSecretNames.repoSecrets = await gh.listRepoSecrets(owner, repo);
-    console.log(`${owner}/${repo} has ${currentSecretNames.repoSecrets.length} secrets: ${currentSecretNames.repoSecrets}`);
-
-    await Promise.all(Object.keys(stacks).map(async (stackName) => {
-      const environment = stacks[stackName];
-      const envSecrets = await gh.listEnvironmentSecrets(owner, repo, environment);
-      currentSecretNames.environmentSecrets[environment] = envSecrets;
-      console.log(`${owner}/${repo}/${environment} has ${envSecrets.length} secrets: ${envSecrets}`);
-    }));
-
-    // List the current variables
-    currentVariableNames.repoVariables = await gh.listRepoVariables(owner, repo);
-    console.log(`${owner}/${repo} has ${currentVariableNames.repoVariables.length} variables: ${currentVariableNames.repoVariables}`);
-
-    await Promise.all(Object.keys(stacks).map(async (stackName) => {
-      const environment = stacks[stackName];
-      const envVars = await gh.listEnvironmentVariables(owner, repo, environment);
-      currentVariableNames.environmentVariables[environment] = envVars;
-      console.log(`${owner}/${repo}/${environment} has ${envVars.length} variables: ${envVars}`);
-    }));
-
-    // Parse the input json (read the secrets and variables that we intend to set on the repo and environments)
-    const newSecrets = await readSecrets();
-    const newVariables = await readVariables();
-
-    // Confirm the list of variables to be set
-    console.log(`Variables to be set: ${JSON.stringify(newVariables, null, 2)}`);
-    const newSecretNames: Record<string, any> = {};
-    newSecretNames.repoSecrets = Object.keys(newSecrets.repoSecrets);
-    newSecretNames.environmentSecrets = {};
-    Object.keys(newSecrets.environmentSecrets).forEach((environment) => {
-      newSecretNames.environmentSecrets[environment] = Object.keys(newSecrets.environmentSecrets[environment]);
-    });
-    console.log(`Secrets (names) to be set: ${JSON.stringify(newSecretNames, null, 2)}`);
-
-    // Calculate any leftover secrets or variables (existing in GHA but not in our infrastructure code)
-    const leftoverSecretNames = {
-      repoSecrets: <string[]>[],
-      environmentSecrets: <{[environment:string]:string[]}>{},
-    };
-
-    leftoverSecretNames.repoSecrets = currentSecretNames.repoSecrets.filter((item) => !Object.keys(newSecrets.repoSecrets).includes(item));
-    Object.keys(currentSecretNames.environmentSecrets).forEach((environment) => {
-      leftoverSecretNames.environmentSecrets[environment] = currentSecretNames.environmentSecrets[environment].filter(
-        (item) => !Object.keys(newSecrets.environmentSecrets[environment]).includes(item),
-      );
-    });
-
-    const leftoverVariableNames = {
-      repoVariables: <string[]>[],
-      environmentVariables: <{[environment:string]:string[]}>{},
-    };
-
-    leftoverVariableNames.repoVariables = currentVariableNames.repoVariables.filter((item) => !Object.keys(newVariables.repoVariables).includes(item));
-    Object.keys(currentVariableNames.environmentVariables).forEach((environment) => {
-      leftoverVariableNames.environmentVariables[environment] = currentVariableNames.environmentVariables[environment].filter(
-        (item) => !Object.keys(newVariables.environmentVariables[environment]).includes(item),
-      );
-    });
-
-    // List out any leftover secrets
-    if (leftoverSecretNames.repoSecrets.length > 0) {
-      console.log(` * NB: Some repo secrets were not included in the CloudFormation outputs (${leftoverSecretNames.repoSecrets.length}):`);
-      leftoverSecretNames.repoSecrets.forEach((secretName) => console.log(` - ${secretName}`));
-    }
-    Object.keys(leftoverSecretNames.environmentSecrets).forEach((environment) => {
-      if (leftoverSecretNames.environmentSecrets[environment].length > 0) {
-        console.log(` * NB: Some environment secrets in ${environment} were not included in the CloudFormation outputs (${leftoverSecretNames.environmentSecrets[environment].length}):`);
-        const environmentSecrets = leftoverSecretNames.environmentSecrets[environment];
-        environmentSecrets.forEach((secretName) => console.log(` - ${secretName}`));
-      }
-    });
-
-    // List out any leftover variables
-    if (leftoverVariableNames.repoVariables.length > 0) {
-      console.log(` * NB: Some repo variables were not included in the CloudFormation outputs (${leftoverVariableNames.repoVariables.length}):`);
-      leftoverVariableNames.repoVariables.forEach((variableName) => console.log(` - ${variableName}`));
-    }
-    Object.keys(leftoverVariableNames.environmentVariables).forEach((environment) => {
-      if (leftoverVariableNames.environmentVariables[environment].length > 0) {
-        console.log(` * NB: Some environment variables in ${environment} were not included in the CloudFormation outputs (${leftoverVariableNames.environmentVariables[environment].length}):`);
-        const environmentVariables = leftoverVariableNames.environmentVariables[environment];
-        environmentVariables.forEach((variableName) => console.log(` - ${variableName}`));
-      }
-    });
-
-    // Delete any leftover secrets
-    await Promise.all(leftoverSecretNames.repoSecrets.map(async (secretName) => gh.deleteRepoSecret(secretName, owner, repo)));
-    await Promise.all(Object.keys(leftoverSecretNames.environmentSecrets).map(async (environment) => {
-      const environmentSecrets = leftoverSecretNames.environmentSecrets[environment];
-      await Promise.all(environmentSecrets.map(async (secretName) => gh.deleteEnvironmentSecret(secretName, owner, repo, environment)));
-    }));
-
-    // Delete any leftover variables
-    await Promise.all(leftoverVariableNames.repoVariables.map(async (variableName) => gh.deleteRepoVariable(variableName, owner, repo)));
-    await Promise.all(Object.keys(leftoverVariableNames.environmentVariables).map(async (environment) => {
-      const environmentVariables = leftoverVariableNames.environmentVariables[environment];
-      await Promise.all(environmentVariables.map(async (secretName) => gh.deleteEnvironmentVariable(secretName, owner, repo, environment)));
-    }));
-
-    // Set secrets
-    await Promise.all(Object.keys(newSecrets.repoSecrets).map(async (secretName) => gh.setRepoSecret(secretName, newSecrets.repoSecrets[secretName], owner, repo)));
-    await Promise.all(Object.keys(newSecrets.environmentSecrets).map(async (environment) => {
-      const environmentSecrets = newSecrets.environmentSecrets[environment];
-      Object.keys(environmentSecrets).map(async (secretName) => gh.setEnvironmentSecret(secretName, environmentSecrets[secretName], owner, repo, environment));
-    }));
-
-    // Set variables
-    await Promise.all(Object.keys(newVariables.repoVariables).map(async (variableName) => gh.setRepoVariable(variableName, newVariables.repoVariables[variableName], owner, repo)));
-    await Promise.all(Object.keys(newVariables.environmentVariables).map(async (environment) => {
-      const environmentVariables = newVariables.environmentVariables[environment];
-      Object.keys(environmentVariables).map(async (variableName) => gh.setEnvironmentVariable(variableName, environmentVariables[variableName], owner, repo, environment));
-    }));
+    const newVariables = await processVariables();
+    const newSecrets = await processSecrets();
 
     // Useful for debugging secret values being passed to Github:
     writeFileSync('secrets/gha_secrets.txt', JSON.stringify(newSecrets, null, 2));
