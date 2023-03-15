114b9c4c4f7a76a679b712558dc8269e9d635cae
David <david.carboni@policyinpractice.co.uk>
2023-03-15 11:32:40 +0000

    Enable setting secrets per-environment

--- infrastructure/src/github/repo.ts
+++ infrastructure/src/github/repo.ts
@@ -26,6 +26,10 @@ export async function getRepo(owner: string, repo: string): Promise<any> {
   throw new Error('Error getting repo information');
 }
 
+export async function getRepoId(owner: string, repo: string) {
+  return (await octokit.repos.get({ owner, repo })).data.id;
+}
+
 export async function getRepoPublicKey(owner: string, repo: string) {
   if (repoPublicKey) return repoPublicKey;
 
@@ -73,7 +77,36 @@ export async function listRepoSecrets(owner: string, repo: string): Promise<stri
   return names;
 }
 
-export async function setSecret(
+export async function listEnvironmentSecrets(owner: string, repo: string, environment: string): Promise<string[]> {
+  const response = await octokit.actions.listEnvironmentSecrets({
+    environment_name: environment,
+    repository_id: await getRepoId(owner, repo),
+    per_page: 100,
+  });
+
+  if (response.status !== 200) {
+    console.log(response);
+    throw new Error(`Error listing environment ${environment} secrets`);
+  }
+
+  const { secrets } = response.data;
+  const totalCount = response.data.total_count;
+
+  // Check we've got all the secrets
+  if (totalCount > 100) {
+    throw new Error('Too many secrets, need to paginate.');
+  }
+
+  // Collate secret names
+  const names: string[] = [];
+  secrets.forEach((secret) => {
+    names.push(secret.name);
+  });
+
+  return names;
+}
+
+export async function setRepoSecret(
   secretName: string,
   secretValue: string,
   owner: string,
@@ -98,7 +131,34 @@ export async function setSecret(
   throw new Error(`Error setting secret value: ${secretName}: status code ${response.status}`);
 }
 
-export async function setVariable(
+export async function setEnvironmentSecret(
+  secretName: string,
+  secretValue: string,
+  owner: string,
+  repo: string,
+  environment: string,
+): Promise<string> {
+  if (!secretValue) throw new Error(`No value for secret ${secretName}`);
+  const publicKey = await getRepoPublicKey(owner, repo);
+  const encryptedValue = await encrypt(secretValue, publicKey.key);
+  const repoId = await getRepoId(owner, repo);
+  const response = await octokit.actions.createOrUpdateEnvironmentSecret({
+    environment_name: environment,
+    repository_id: repoId,
+    secret_name: secretName,
+    encrypted_value: encryptedValue,
+    key_id: publicKey.key_id,
+  });
+
+  if (response.status === 201 || response.status === 204) {
+    return secretName;
+  }
+  // Looks like that didn't work.
+  console.log(response);
+  throw new Error(`Error setting environment ${environment} secret value: ${secretName}: status code ${response.status}`);
+}
+
+export async function setRepoVariable(
   name: string,
   value: string,
   owner: string,
@@ -117,10 +177,33 @@ export async function setVariable(
   }
   // Looks like that didn't work.
   console.log(response);
-  throw new Error(`Error setting secret value: ${name}: status code ${response.status}`);
+  throw new Error(`Error setting environment variable value: ${name}: status code ${response.status}`);
 }
 
-export async function deleteSecret(
+export async function setEnvironmentVariable(
+  name: string,
+  value: string,
+  owner: string,
+  repo: string,
+  environment: string,
+): Promise<string> {
+  if (!value) throw new Error(`No value for secret ${name}`);
+  const response = await octokit.rest.actions.createEnvironmentVariable({
+    environment_name: environment,
+    repository_id: await getRepoId(owner, repo),
+    name,
+    value,
+  });
+
+  if (response.status === 201 || response.status === 204) {
+    return name;
+  }
+  // Looks like that didn't work.
+  console.log(response);
+  throw new Error(`Error setting environment ${environment} variable value: ${name}: status code ${response.status}`);
+}
+
+export async function deleteRepoSecret(
   secretName: string,
   owner: string,
   repo: string,
@@ -137,4 +220,24 @@ export async function deleteSecret(
   // Looks like that didn't work.
   console.log(response);
   throw new Error(`Error deleting secret value: ${secretName}: status code ${response.status}`);
+}
+
+export async function deleteEnvironmentSecret(
+  secretName: string,
+  owner: string,
+  repo: string,
+  environment: string,
+): Promise<string> {
+  const response = await octokit.rest.actions.deleteEnvironmentSecret({
+    environment_name: environment,
+    repository_id: await getRepoId(owner, repo),
+    secret_name: secretName,
+  });
+
+  if (response.status === 204) {
+    return secretName;
+  }
+  // Looks like that didn't work.
+  console.log(response);
+  throw new Error(`Error deleting environment ${environment} secret value: ${secretName}: status code ${response.status}`);
 }
--- infrastructure/src/github/secrets.ts
+++ infrastructure/src/github/secrets.ts
@@ -1,4 +1,4 @@
-import { existsSync, readFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import * as gh from './repo';
 
 // Any Cludformation outputs that don't need to be added as GHA secrets:
@@ -6,6 +6,7 @@ const skipList = [
   'queueArn',
   'metricsSenderPolicyArn',
   'githubWebhook',
+  'ExportsOutput',
 ];
 
 // Any secrets to be added from environment variables passed to this script:
@@ -13,58 +14,75 @@ const shellEnvSecrets: string[] = [
   // 'SLACK_WEBHOOK',
 ];
 
+const stacks : Record<string, string> = {
+  CalculatorProduction: 'production',
+  CalculatorStaging: 'staging',
+};
+
 // Values from /secrets/github.sh
 const owner = process.env.OWNER || process.env.USERNAME || '';
 const repo = process.env.REPO || '';
 if (!owner) throw new Error('No repo owner: please set an environment variable for either USERNAME or OWNER');
 if (!repo) throw new Error('No repo: please set an environment variable for REPO');
 
-let repoSecrets: string[];
+const leftoverSecretNames: {repoSecrets:string[], environmentSecrets: Record<string, string[]>} = { repoSecrets: [], environmentSecrets: {} };
 
-function readSecrets(): { [key: string]: string; } {
-  const secrets: { [key: string]: string; } = {};
+interface Secrets {
+  [name: string]: string
+}
 
-  const cdkOuputs = 'secrets/cdk-outputs.json';
+function readSecrets(): {repoSecrets:Secrets, environmentSecrets: Record<string, Secrets>} {
+  const secrets: {repoSecrets:Secrets, environmentSecrets: Record<string, Secrets>} = { repoSecrets: {}, environmentSecrets: {} };
+
+  const cdkOutputs = 'secrets/cdk-outputs.json';
   // const awsConfig = '~/.aws/credentials';
-  if (existsSync(cdkOuputs)) {
-    const json = readFileSync(cdkOuputs, 'utf8').trim();
+  if (existsSync(cdkOutputs)) {
+    const json = readFileSync(cdkOutputs, 'utf8').trim();
     const outputs = JSON.parse(json);
-    const stackKeys = Object.keys(outputs);
-    if (stackKeys.length === 1) {
-      // Convert camel-case keys to env-var style for GH secret names:
-      const keys = outputs[stackKeys[0]];
-      Object.keys(keys).forEach((key: any) => {
-        if (skipList.filter((skip) => key.startsWith(skip)).length > 0) {
-          console.log(` x : ${key}`);
-        } else {
+    const stackNames = Object.keys(outputs);
+    if (stackNames.length > 0) {
+      stackNames.forEach((stackName) => {
+        const environmentName = stacks[stackName];
+        secrets.environmentSecrets[environmentName] = {};
+        // Convert camel-case keys to env-var style for GH secret names:
+        const keys = outputs[stackName];
+        Object.keys(keys).forEach((key: any) => {
+          if (skipList.filter((skip) => key.startsWith(skip)).length > 0) {
+            console.log(` x : ${key}`);
+          } else {
           // https://stackoverflow.com/questions/54246477/how-to-convert-camelcase-to-snake-case-in-javascript
           // e.g. cfnOutputName -> CFN_OUTPUT_NAME
-          const snakeCase: string = key.replace(/[A-Z]/g, (letter: string) => `_${letter.toLowerCase()}`);
-          const name = snakeCase.toUpperCase();
-          const value = keys[key];
-          secrets[name] = value;
-        }
+            const snakeCase: string = key.replace(/[A-Z]/g, (letter: string) => `_${letter.toLowerCase()}`);
+            const name = snakeCase.toUpperCase();
+            const value = keys[key];
+            if (environmentName) {
+              secrets.environmentSecrets[environmentName][name] = value;
+            } else {
+              secrets.repoSecrets[name] = value;
+            }
+          }
+        });
       });
 
       // Secrets from shell variables
       shellEnvSecrets.forEach((shellEnvSecret) => {
-        secrets[shellEnvSecret] = process.env[shellEnvSecret] || '';
+        secrets.repoSecrets[shellEnvSecret] = process.env[shellEnvSecret] || '';
       });
 
       // Confirm the list of secrets to be set
-      Object.keys(secrets).forEach((key) => {
-        console.log(` - ${key}`);
-      });
-      console.log(`(${Object.keys(secrets).length})`);
+      console.log(`Secrets to be set: ${JSON.stringify(secrets, null, 2)}`);
 
       // Work out whether there are any "leftover" secrets on the repo that we've not got values for
-      repoSecrets = repoSecrets.filter((item) => !Object.keys(secrets).includes(item));
+      leftoverSecretNames.repoSecrets = leftoverSecretNames.repoSecrets.filter((item) => !Object.keys(secrets.repoSecrets).includes(item));
+      Object.keys(leftoverSecretNames.environmentSecrets).forEach((environment) => {
+        leftoverSecretNames.environmentSecrets[environment].filter((item) => !Object.keys(secrets.environmentSecrets[environment]).includes(item));
+      });
 
       return secrets;
     }
     throw new Error('No output keys found from CDK');
   }
-  throw new Error(`Couldn't find file ${cdkOuputs}`);
+  throw new Error(`Couldn't find file ${cdkOutputs}`);
 }
 
 (async () => {
@@ -74,33 +92,46 @@ function readSecrets(): { [key: string]: string; } {
     await gh.getRepoPublicKey(owner, repo);
 
     // List the current secrets
-    repoSecrets = await gh.listRepoSecrets(owner, repo);
-    console.log(`${owner}/${repo} has ${repoSecrets.length} secrets: ${repoSecrets}`);
+    leftoverSecretNames.repoSecrets = await gh.listRepoSecrets(owner, repo);
+    console.log(`${owner}/${repo} has ${leftoverSecretNames.repoSecrets.length} secrets: ${leftoverSecretNames.repoSecrets}`);
+
+    await Promise.all(Object.keys(stacks).map(async (stackName) => {
+      const environment = stacks[stackName];
+      leftoverSecretNames.environmentSecrets[environment] = await gh.listEnvironmentSecrets(owner, repo, environment);
+    }));
 
     // Parse the input json
     const secrets = readSecrets();
-    if (repoSecrets.length > 0) {
-      console.log(` * NB: Some secrets were not included in the CloudFormation outputs (${repoSecrets.length}):`);
-      repoSecrets.forEach((secretName) => {
-        console.log(` - ${secretName}`);
-        // Uncomment this to delete unrecognised secrets:
-        gh.deleteSecret(secretName, owner, repo);
-      });
+
+    // List out any leftover secrets
+    if (leftoverSecretNames.repoSecrets.length > 0) {
+      console.log(` * NB: Some repo secrets were not included in the CloudFormation outputs (${leftoverSecretNames.repoSecrets.length}):`);
+      leftoverSecretNames.repoSecrets.forEach((secretName) => console.log(` - ${secretName}`));
     }
-
-    // Github secrets
-    const promises: Promise<string>[] = [];
-    Object.keys(secrets).forEach((secretName) => {
-      const promise = gh.setSecret(secretName, secrets[secretName], owner, repo);
-      promises.push(promise);
+    Object.keys(leftoverSecretNames.environmentSecrets).forEach((environment) => {
+      if (leftoverSecretNames.environmentSecrets[environment].length > 0) {
+        console.log(` * NB: Some environment secrets in ${environment} were not included in the CloudFormation outputs (${leftoverSecretNames.environmentSecrets[environment].length}):`);
+        const environmentSecrets = leftoverSecretNames.environmentSecrets[environment];
+        environmentSecrets.forEach((secretName) => console.log(` - ${secretName}`));
+      }
     });
-    const result = await Promise.all(promises);
-    console.log(`Set ${result.length} secrets: ${JSON.stringify(result)}`);
 
-    // TODO: Delete "leftover" secrets?
+    // Delete any leftover secrets
+    await Promise.all(leftoverSecretNames.repoSecrets.map(async (secretName) => gh.deleteRepoSecret(secretName, owner, repo)));
+    await Promise.all(Object.keys(leftoverSecretNames.environmentSecrets).map(async (environment) => {
+      const environmentSecrets = leftoverSecretNames.environmentSecrets[environment];
+      await Promise.all(environmentSecrets.map(async (secretName) => gh.deleteEnvironmentSecret(secretName, owner, repo, environment)));
+    }));
+
+    // Set secrets
+    await Promise.all(Object.keys(secrets.repoSecrets).map(async (secretName) => gh.setRepoSecret(secretName, secrets.repoSecrets[secretName], owner, repo)));
+    await Promise.all(Object.keys(secrets.environmentSecrets).map(async (environment) => {
+      const environmentSecrets = secrets.environmentSecrets[environment];
+      Object.keys(environmentSecrets).map(async (secretName) => gh.setEnvironmentSecret(secretName, environmentSecrets[secretName], owner, repo, environment));
+    }));
 
     // Useful for debugging secret values being passed to Github:
-    // writeFileSync('secrets/gha_secrets.txt', JSON.stringify(secrets));
+    writeFileSync('secrets/gha_secrets.txt', JSON.stringify(secrets, null, 2));
   } catch (err) {
     console.error(err);
     throw err;
