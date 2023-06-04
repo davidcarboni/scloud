71f65c8fba3ec4b270b84415bc7ba642771847ba
David Carboni <david@carboni.io>
2023-06-04 21:29:44 +0100

    StackOverflow reference for parsing body as text

--- llocal/src/llocal.ts
+++ llocal/src/llocal.ts
@@ -5,6 +5,8 @@ import { handler } from './lambda';
 
 const port = 3000;
 const app = express();
+
+// https://stackoverflow.com/questions/12345166/how-to-force-parse-request-body-as-plain-text-instead-of-json-in-express
 app.use(express.text({ type: '*/*' }));
 
 app.get('/*', async (req: Request, res: Response) => {
--- slack/src/llocal.ts
+++ slack/src/llocal.ts
@@ -7,6 +7,8 @@ import { handler } from './lambda';
 
 const port = 3000;
 const app = express();
+
+// https://stackoverflow.com/questions/12345166/how-to-force-parse-request-body-as-plain-text-instead-of-json-in-express
 app.use(express.text({ type: '*/*' }));
 
 app.post('/*', async (req: Request, res: Response) => {
