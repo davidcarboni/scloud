eb96b2d960276a91bd2efa911ebf04f71f6142b0
David Carboni <david@carboni.io>
2023-06-09 23:39:02 -0400

    SOme git/github glitch

--- infrastructure/src/github/repo.ts
+++ infrastructure/src/github/repo.ts
@@ -26,10 +26,6 @@ export async function getRepo(owner: string, repo: string): Promise<any> {
   throw new Error('Error getting repo information');
 }
 
-export async function getRepoId(owner: string, repo: string) {
-  return (await octokit.repos.get({ owner, repo })).data.id;
-}
-
 export async function getRepoPublicKey(owner: string, repo: string) {
   if (repoPublicKey) return repoPublicKey;
 
@@ -107,36 +103,6 @@ export async function listRepoVariables(owner: string, repo: string): Promise<st
 }
 
 export async function setRepoSecret(
-export async function listEnvironmentSecrets(owner: string, repo: string, environment: string): Promise<string[]> {
-  const response = await octokit.actions.listEnvironmentSecrets({
-    environment_name: environment,
-    repository_id: await getRepoId(owner, repo),
-    per_page: 100,
-  });
-
-  if (response.status !== 200) {
-    console.log(response);
-    throw new Error(`Error listing environment ${environment} secrets`);
-  }
-
-  const { secrets } = response.data;
-  const totalCount = response.data.total_count;
-
-  // Check we've got all the secrets
-  if (totalCount > 100) {
-    throw new Error('Too many secrets, need to paginate.');
-  }
-
-  // Collate secret names
-  const names: string[] = [];
-  secrets.forEach((secret) => {
-    names.push(secret.name);
-  });
-
-  return names;
-}
-
-export async function setRepoSecret(
   secretName: string,
   secretValue: string,
   owner: string,
@@ -205,38 +171,9 @@ export async function setRepoVariable(
 
     throw new Error(`Error setting repo variable value: ${name}: status code ${response.status}`);
   }
-  if (response.status === 201 || response.status === 204) {
-    return name;
-  }
-  // Looks like that didn't work.
-  console.log(response);
-  throw new Error(`Error setting environment variable value: ${name}: status code ${response.status}`);
 }
 
-export async function setEnvironmentVariable(
-  name: string,
-  value: string,
-  owner: string,
-  repo: string,
-  environment: string,
-): Promise<string> {
-  if (!value) throw new Error(`No value for secret ${name}`);
-  const response = await octokit.rest.actions.createEnvironmentVariable({
-    environment_name: environment,
-    repository_id: await getRepoId(owner, repo),
-    name,
-    value,
-  });
-
-  if (response.status === 201 || response.status === 204) {
-    return name;
-  }
-  // Looks like that didn't work.
-  console.log(response);
-  throw new Error(`Error setting environment ${environment} variable value: ${name}: status code ${response.status}`);
-}
-
-export async function deleteRepoRepoSecret(
+export async function deleteRepoSecret(
   secretName: string,
   owner: string,
   repo: string,
