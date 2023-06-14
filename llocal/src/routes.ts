1dccceb756dc7fd8da5be89917e6bc8d28f54612
David Carboni <david@carboni.io>
2023-06-14 13:54:54 +0100

    Retired now we have lambda-local

--- llocal/src/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Routes } from './types';
-
-const routes: Routes = {
-  '/api/ping': { GET: async (request: Request) => ({ statusCode: 200, body: request }) },
-};
-export default routes;
