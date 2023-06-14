7ffa5c7543c050dd5c4bdab3126e20ad40bacf3c
David Carboni <david@carboni.io>
2023-06-14 13:53:30 +0100

    Retired the accelerate component

--- accelerate/test/lambda.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
-// import * as l from '../src/lambda';
-
-describe('Some test', () => {
-  it('Should meet expectation', () => {
-    expect(undefined).to.be.undefined;
-  });
-});
