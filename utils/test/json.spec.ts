3c635ba355b2586e2b77927bccbb009be5183cb3
David <david.carboni@policyinpractice.co.uk>
2023-06-23 13:59:09 +0100

    Tidy up and standardise

--- utils/test/json.spec.ts
+++ utils/test/json.spec.ts
@@ -1,10 +1,27 @@
 import { expect } from 'chai';
+import { parseJson } from 'json';
 import { describe, it } from 'mocha';
 
 // chai.use(require('chai-as-promised'));
 
-describe('Placeholder test', () => {
-  it('Given a condition, when we take an action, we should get a result', () => {
-    expect(true).to.be.true;
+describe('parseJson', () => {
+  it('Should parse valid json', () => {
+    const result = parseJson('{"key":"value"}');
+    expect(result).to.deep.equal({ key: 'value' });
+  });
+
+  it('Should gracefully parse undefined', () => {
+    const result = parseJson(undefined);
+    expect(result).to.equal(undefined);
+  });
+
+  it('Should gracefully parse empty string', () => {
+    const result = parseJson('');
+    expect(result).to.equal(undefined);
+  });
+
+  it('Should gracefully parse invalid json', () => {
+    const result = parseJson('this is not valid json');
+    expect(result).to.equal(undefined);
   });
 });
