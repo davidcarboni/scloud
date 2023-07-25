04c3c6bbf31a3c9ee90f060c28a9dcb6d0d432fd
David <david.carboni@policyinpractice.co.uk>
2023-07-25 18:34:39 +0100

    Fix

--- lambda-local/src/index.ts
+++ lambda-local/src/index.ts
@@ -1,5 +1,4 @@
 export { sqsLocal } from './sqsLocal';
-export { apiGatewayLocal } from './apiGatewayLocal';
 export { cloudfrontLocal } from './cloudfrontLocal';
 export { webappLocal } from './webappLocal';
 export { webappRoutesLocal } from './webappRoutesLocal';
