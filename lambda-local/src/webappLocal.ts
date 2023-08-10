577244f8e459810adb06cea22e753dd489b8fe5f
Joe Elleson <joe.elleson@gmail.com>
2023-08-10 10:54:51 +0100

    increase payload max size for local webapp

--- lambda-local/src/webappLocal.ts
+++ lambda-local/src/webappLocal.ts
@@ -77,8 +77,9 @@ export function webappLocal(
   const port = +(process.env.PORT || '3000');
   const app = express();
 
+
   // https://stackoverflow.com/questions/12345166/how-to-force-parse-request-body-as-plain-text-instead-of-json-in-express
-  app.use(express.text({ type: '*/*' }));
+  app.use(express.text({ type: '*/*', limit: '50mb' }));
 
   // "Static bucket" serving
   if (staticContent) app.use(staticContent.appPath, express.static(staticContent.sourceDirectory));
