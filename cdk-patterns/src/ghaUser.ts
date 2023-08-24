955f74d8a3713e3d4d057af0619649b227a85f26
David <david.carboni@policyinpractice.co.uk>
2023-08-24 12:23:56 +0100

    Draft for ghaOidc

--- cdk-patterns/src/ghaUser.ts
+++ cdk-patterns/src/ghaUser.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 import * as fs from 'fs';
 import {
-  CfnAccessKey, ManagedPolicy, PolicyStatement, User,
+  CfnAccessKey, Conditions, ManagedPolicy, OpenIdConnectProvider, PolicyStatement, Role, User, WebIdentityPrincipal,
 } from 'aws-cdk-lib/aws-iam';
 import { CfnOutput, Stack } from 'aws-cdk-lib';
 import { IRepository } from 'aws-cdk-lib/aws-ecr';
@@ -90,28 +90,10 @@ function addToPolicy(stack: Stack, name: string, policy: ManagedPolicy, resource
   }
 }
 
-/**
- * A user for Gihud Actions CI/CD.
- */
-export function ghaUser(stack: Stack): { user: User, accessKey: CfnAccessKey | undefined; } {
-  // A user with the policy attached
-  const user = new User(stack, 'ghaUser', { userName: `gha-${stack.stackName}` });
-  const policy = new ManagedPolicy(stack, `gha-${stack.stackName}-policy`, {
-    managedPolicyName: `gha-${stack.stackName}-policy`,
+export function ghaPolicy(stack: Stack, name: string = `gha-${stack.stackName}-policy`) {
+  const policy = new ManagedPolicy(stack, name, {
+    managedPolicyName: name,
   });
-  user.addManagedPolicy(policy);
-
-  // Credentials
-  let accessKey: CfnAccessKey | undefined;
-  if (!process.env.REKEY) {
-    accessKey = new CfnAccessKey(stack, 'ghaUserAccessKey', {
-      userName: user.userName,
-    });
-
-    // Access key details for GHA secrets
-    addGhaSecret(stack, 'awsAccessKeyId', accessKey.ref);
-    addGhaSecret(stack, 'awsSecretAccessKey', accessKey.attrSecretAccessKey);
-  }
 
   // ECR repositories - push/pull images
   const repositoryArns = ghaInfo.resources.repositories
@@ -168,6 +150,79 @@ export function ghaUser(stack: Stack): { user: User, accessKey: CfnAccessKey | u
   addToPolicy(stack, 'distributions', policy, distributionArns, [
     'cloudfront:CreateInvalidation',
   ]);
+
+  if (fs.existsSync('secrets')) {
+    // Write out the list of secret and variable names:
+    fs.writeFileSync(`secrets/${stack.stackName}.ghaSecrets.json`, JSON.stringify(ghaInfo.secrets));
+    fs.writeFileSync(`secrets/${stack.stackName}.ghaVariables.json`, JSON.stringify(ghaInfo.variables));
+  }
+
+  // Flush ghaInfo so we're free to build another stack if needed:
+  ghaInfo.resources.buckets = [];
+  ghaInfo.resources.distributions = [];
+  ghaInfo.resources.lambdas = [];
+  ghaInfo.resources.repositories = [];
+  ghaInfo.resources.services = [];
+  ghaInfo.secrets = [];
+  ghaInfo.variables = [];
+
+  return policy;
+}
+
+/**
+ * Create an OIDC connection fo Guthub Actions
+ * See: https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services
+ * @param repo The repository to grant access to (owner and name). You can also specify a filter to limit access e.g. to a branch.
+ */
+export function ghaOidc(stack: Stack, repo: { owner: string, name: string; filter?: string; }) {
+  const ghProvider = new OpenIdConnectProvider(stack, `gha-oidc-${stack.stackName}`, {
+    url: 'https://token.actions.githubusercontent.com',
+    clientIds: ['sts.amazonaws.com'],
+  });
+
+  // grant only requests coming from the specific GitHub repository.
+  const condition = `repo:${repo.owner}/${repo.name}:${repo.filter || '*'}`;
+  const conditions: Conditions = {
+    StringLike: {
+      'token.actions.githubusercontent.com:sub': [condition],
+    },
+  };
+
+  const role = new Role(stack, `gha-oidc-role-${stack.stackName}`, {
+    assumedBy: new WebIdentityPrincipal(
+      ghProvider.openIdConnectProviderArn,
+      conditions,
+    ),
+    managedPolicies: [
+      ghaPolicy(stack),
+    ],
+    roleName: `gha-oidc-${stack.stackName}`,
+    description: `Role for GitHub Actions (${stack.stackName}) to assume when deploying to AWS`,
+  });
+
+  addGhaVariable(stack, 'ghaOidc', 'Role', role.roleName);
+}
+
+/**
+ * A user for Gihud Actions CI/CD.
+ */
+export function ghaUser(stack: Stack): { user: User, accessKey: CfnAccessKey | undefined; } {
+  // A user with the policy attached
+  const user = new User(stack, 'ghaUser', { userName: `gha-${stack.stackName}` });
+  const policy = ghaPolicy(stack);
+  user.addManagedPolicy(policy);
+
+  // Credentials
+  let accessKey: CfnAccessKey | undefined;
+  if (!process.env.REKEY) {
+    accessKey = new CfnAccessKey(stack, 'ghaUserAccessKey', {
+      userName: user.userName,
+    });
+
+    // Access key details for GHA secrets
+    addGhaSecret(stack, 'awsAccessKeyId', accessKey.ref);
+    addGhaSecret(stack, 'awsSecretAccessKey', accessKey.attrSecretAccessKey);
+  }
 
   if (fs.existsSync('secrets')) {
     // Write out the list of secret and variable names:
