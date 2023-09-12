8e0b8ea6dfd7556219c4038dcd64e04d4e49c790
David Carboni <david@carboni.io>
2023-09-12 20:44:25 +0100

    Export Bucket and Scheduled Lambdas as Constructs

--- cdk-patterns/src/BucketLambdaContainer.ts
+++ cdk-patterns/src/BucketLambdaContainer.ts
@@ -1,46 +1,51 @@
-import { Function, FunctionProps } from 'aws-cdk-lib/aws-lambda';
+import { DockerImageFunctionProps, Function, FunctionProps } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { Bucket, BucketProps, EventType } from 'aws-cdk-lib/aws-s3';
+import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { PrivateBucket } from './PrivateBucket';
-import { ZipFunction } from './ZipFunction';
+import { ContainerFunction } from './ContainerFunction';
 
 /**
- * A Lambda function triggered by s3 bucket events.
+ * A container Lambda function triggered by s3 bucket events.
  *
- * Defaults for the bucket are as per PrivateBucket:
+ * Defaults for the bucket are as per privateBucket:
  *  - encryption: BucketEncryption.S3_MANAGED
  *  = blockPublicAccess: BlockPublicAccess.BLOCK_ALL
  *  - removalPolicy: RemovalPolicy.DESTROY
  *
  * NB By default, Cloudformation will attempt to destroy the bucket when the stack is destroyed,
  * however we don't set auto-delete objects to true, so this will fail if the bucket is not empty,
- * which prevents losing content,
+ * which is a good thing for not losing content.
  *
- * The reason for this is that when experimenting with building stacks you can end up with a
+ * The reason for this is that when experimenting with building stacks you can end up with quite a
  * bunch of orphaned resources. This setting effectively protects buckets that have content in them
  * But allows for deleting an empty bucket.
  *
  * If you want to delete the bucket and all contents, pass { autoDeleteObjects: true } in bucketProps.
  */
-export class BucketLambda extends Construct {
-  bucket: Bucket;
+export class BucketLambdaContainer extends Construct {
+  containerFunction: ContainerFunction;
 
-  lambda: Function;
+  bucket: Bucket;
 
   constructor(
     scope: Construct,
     id: string,
     environment?: { [key: string]: string; },
-    lambdaProps?: Partial<FunctionProps>,
+    lambdaProps?: Partial<DockerImageFunctionProps>,
     bucketProps?: Partial<BucketProps>,
+    tagOrDigest?: string,
+    ecr?: Repository,
     events: EventType[] = [EventType.OBJECT_CREATED],
+    initialPass: boolean = false,
   ) {
-    super(scope, `${id}BucketLambda`);
+    super(scope, `${id}BucketLambdaContainer`);
+
     // Triggering bucket
     this.bucket = new PrivateBucket(scope, `${id}Bucket`, bucketProps);
 
-    this.lambda = new ZipFunction(scope, id, environment, { ...lambdaProps });
-    this.lambda.addEventSource(new S3EventSource(this.bucket, { events }));
+    this.containerFunction = new ContainerFunction(scope, id, environment, { ...lambdaProps }, tagOrDigest, ecr, initialPass);
+    this.containerFunction.lambda.addEventSource(new S3EventSource(this.bucket, { events }));
   }
 }
--- cdk-patterns/src/ScheduledLambda.ts
+++ cdk-patterns/src/ScheduledLambda.ts
@@ -1,10 +1,8 @@
-import { DockerImageFunctionProps, Function, FunctionProps } from 'aws-cdk-lib/aws-lambda';
+import { FunctionProps } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 import { ZipFunction } from './ZipFunction';
-import { ContainerFunction } from './ContainerFunction';
 
 // Based on: https://edwinradtke.com/eventtargets
 
@@ -14,45 +12,25 @@ import { ContainerFunction } from './ContainerFunction';
  * The default schedule isSchedule.cron({ minute: '11', hour: '1' })
  * Which sets '11 1 * * ? *' (i.e. 1:11am every day)
  */
-export function bucketLambda(
-  construct: Construct,
-  name: string,
-  environment?: { [key: string]: string; },
-  lambdaProps?: Partial<FunctionProps>,
-  schedule: Schedule = Schedule.cron({ minute: '11', hour: '1' }),
-): { rule: Rule, lambda: Function; } {
-  const lambda = new ZipFunction(construct, name, environment, { ...lambdaProps });
+export class ScheduledLambda extends Construct {
+  zipFunction: ZipFunction;
 
-  const rule = new Rule(construct, `${name}Rule`, {
-    schedule,
-    targets: [new LambdaFunction(lambda)],
-  });
+  rule: Rule;
 
-  return {
-    rule, lambda,
-  };
-}
+  constructor(
+    scope: Construct,
+    id: string,
+    environment?: { [key: string]: string; },
+    lambdaProps?: Partial<FunctionProps>,
+    schedule: Schedule = Schedule.cron({ minute: '11', hour: '1' }),
+  ) {
+    super(scope, `${id}ScheduledLambda`);
 
-/**
- * A container Lambda function triggered by scheduled Cloudwatch events.
- */
-export function bucketLambdaContainer(
-  construct: Construct,
-  name: string,
-  initialPass: boolean,
-  environment?: { [key: string]: string; },
-  ecr?: Repository,
-  lambdaProps?: Partial<DockerImageFunctionProps>,
-  schedule: Schedule = Schedule.cron({ minute: '11', hour: '1' }),
-): { repository: Repository, rule: Rule, lambda: Function; } {
-  const { repository, lambda } = new ContainerFunction(construct, name, environment, { ...lambdaProps }, 'latest', ecr, initialPass);
+    this.zipFunction = new ZipFunction(scope, id, environment, { ...lambdaProps });
 
-  const rule = new Rule(construct, `${name}Rule`, {
-    schedule,
-    targets: [new LambdaFunction(lambda)],
-  });
-
-  return {
-    repository, rule, lambda,
-  };
+    this.rule = new Rule(scope, `${id}Rule`, {
+      schedule,
+      targets: [new LambdaFunction(this.zipFunction)],
+    });
+  }
 }
