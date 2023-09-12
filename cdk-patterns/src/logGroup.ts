2570b2325a8c306abdbb96e615f6c17b72516c9c
David Carboni <david@carboni.io>
2023-09-12 20:43:28 +0100

    Tidy up and update to 2-year retention

--- cdk-patterns/src/logGroup.ts
+++ cdk-patterns/src/logGroup.ts
@@ -6,18 +6,18 @@ import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
  * @param stack Parent CDK stack
  * @param category Used to build up the logGroupName: /stackName/categpry/name
  * @param name Name (and base for an ID) for the logGroup
- * @param retention Defaults to RetentionDays.ONE_MONTH
+ * @param retention Defaults to RetentionDays.TWO_YEARS
  */
 export function logGroup(
   stack: Stack,
   category: string,
   name: string,
-  retention: RetentionDays = RetentionDays.EIGHTEEN_MONTHS,
+  retention: RetentionDays = RetentionDays.TWO_YEARS,
 ) : LogGroup {
   return new LogGroup(stack, `${name}LogGroup`, {
-  // Ensure the log group is deleted when the stack is deleted
-  // and that logs aren't retained indefinitely
-    logGroupName: `/${stack.stackName}/ecs/${name}`,
+    // Ensure the log group is deleted when the stack is deleted
+    // and that logs aren't retained indefinitely
+    logGroupName: `/${stack.stackName}/${category}/${name}`,
     removalPolicy: RemovalPolicy.DESTROY,
     retention,
   });
