ee5b8a82e129b8fc09bf60c27ac2d712fd64aaf5
David Carboni <david@carboni.io>
2023-09-15 00:06:49 +0100

    Construc version of GHA functions

---
+++ cdk-patterns/src/GithubActions.ts
@@ -0,0 +1,264 @@
+/* eslint-disable no-param-reassign */
+import * as fs from 'fs';
+import {
+  CfnAccessKey, ManagedPolicy, OpenIdConnectProvider, PolicyStatement, Role, User, WebIdentityPrincipal,
+} from 'aws-cdk-lib/aws-iam';
+import { CfnOutput, Stack } from 'aws-cdk-lib';
+import { IRepository } from 'aws-cdk-lib/aws-ecr';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { IFargateService } from 'aws-cdk-lib/aws-ecs';
+import { IBucket } from 'aws-cdk-lib/aws-s3';
+import { IDistribution } from 'aws-cdk-lib/aws-cloudfront';
+import { Construct } from 'constructs';
+import _ from 'lodash';
+
+/**
+ * Constructs that help integrate GitHub Actions to build and deploy to AWS
+ */
+export class GithubActions extends Construct {
+  id: string;
+
+  stackName: string;
+
+  policy: ManagedPolicy;
+
+  ghaInfo = {
+    resources: {
+      repositories: <IRepository[]>[],
+      buckets: <IBucket[]>[],
+      lambdas: <IFunction[]>[],
+      services: <IFargateService[]>[],
+      distributions: <IDistribution[]>[],
+    },
+    secrets: <string[]>[],
+    variables: <string[]>[],
+  };
+
+  constructor(
+    scope: Construct,
+  ) {
+    super(scope, 'GithubActions');
+    this.stackName = Stack.of(scope).stackName;
+  }
+
+  addGhaSecret(
+    name: string,
+    value: string,
+  ) {
+    const cfnOutput = new CfnOutput(this, name, { value });
+    this.ghaInfo.secrets.push(cfnOutput.node.id);
+  }
+
+  addGhaVariable(
+    construct: Construct,
+    name: string,
+    type: string,
+    value: string,
+  ) {
+    const variableName = `${_.lowerFirst(name)}${_.capitalize(type)}`;
+    const cfnOutput = new CfnOutput(construct, variableName, { value });
+    this.ghaInfo.variables.push(cfnOutput.node.id);
+  }
+
+  addGhaLambda(
+    construct: Construct,
+    name: string,
+    lambda: IFunction,
+  ) {
+    this.ghaInfo.resources.lambdas.push(lambda);
+    this.addGhaVariable(construct, name, 'lambda', lambda.functionName);
+  }
+
+  addGhaBucket(
+    construct: Construct,
+    name: string,
+    bucket: IBucket,
+  ) {
+    this.ghaInfo.resources.buckets.push(bucket);
+    this.addGhaVariable(construct, name, 'bucket', bucket.bucketName);
+  }
+
+  addGhaDistribution(
+    construct: Construct,
+    name: string,
+    distribution: IDistribution,
+  ) {
+    this.ghaInfo.resources.distributions.push(distribution);
+    this.addGhaVariable(construct, name, 'distributionId', distribution.distributionId);
+  }
+
+  addGhaRepository(
+    construct: Construct,
+    name: string,
+    repository: IRepository,
+  ) {
+    this.ghaInfo.resources.repositories.push(repository);
+    this.addGhaVariable(construct, name, 'repository', repository.repositoryName);
+  }
+
+  ghaPolicy() {
+    if (!this.policy) {
+      this.policy = new ManagedPolicy(this, `gha-${this.stackName}-policy`, {
+        managedPolicyName: this.stackName,
+      });
+
+      // ECR repositories - push/pull images
+      const repositoryArns = this.ghaInfo.resources.repositories
+        .filter((repository) => repository)
+        .map((repository) => repository.repositoryArn);
+      if (repositoryArns.length > 0) {
+        this.addToPolicy('ecrLogin', ['*'], ['ecr:GetAuthorizationToken']);
+        this.addToPolicy('ecrRepositories', repositoryArns, [
+          'ecr:GetDownloadUrlForLayer',
+          'ecr:BatchGetImage',
+          'ecr:BatchDeleteImage',
+          'ecr:CompleteLayerUpload',
+          'ecr:UploadLayerPart',
+          'ecr:InitiateLayerUpload',
+          'ecr:BatchCheckLayerAvailability',
+          'ecr:PutImage',
+          'ecr:ListImages',
+        ]);
+      }
+      // Buckets - upload/sync
+      const bucketArns = this.ghaInfo.resources.buckets
+        .filter((bucket) => bucket)
+        .map((bucket) => bucket.bucketArn);
+      this.addToPolicy('buckets', bucketArns, [
+        's3:ListBucket',
+      ]);
+      const bucketObjectsArns = bucketArns.map((arn) => `${arn}/*`);
+      this.addToPolicy('bucketObjects', bucketObjectsArns, [
+        's3:GetObject',
+        's3:PutObject',
+        's3:DeleteObject',
+      ]);
+
+      // Lambdas - update update with a new zip/container build
+      const lambdaArns = this.ghaInfo.resources.lambdas
+        .filter((lambda) => lambda)
+        .map((lambda) => lambda.functionArn);
+      this.addToPolicy('lambdas', lambdaArns, [
+        'lambda:UpdateFunctionCode',
+        // 'lambda:PublishVersion',
+      ]);
+
+      // Fargate services - update with a new container build
+      const serviceArns = this.ghaInfo.resources.services
+        .filter((service) => service)
+        .map((service) => service.serviceArn);
+      this.addToPolicy('fargateServices', serviceArns, [
+        'ecs:UpdateService',
+      ]);
+
+      // Cloudfront distribution - cache invalidation
+      const distributionArns = this.ghaInfo.resources.distributions
+        .filter((distribution) => distribution !== undefined)
+        // Not sure where to 'properly' get a distribution ARN from?
+        .map((distribution) => `arn:aws:cloudfront::${Stack.of(this).account}:distribution/${distribution.distributionId}`);
+      this.addToPolicy('distributions', distributionArns, [
+        'cloudfront:CreateInvalidation',
+      ]);
+    }
+
+    return this.policy;
+  }
+
+  addToPolicy(name: string, resources: string[], actions: string[]) {
+    if (resources.length > 0) {
+      this.policy.addStatements(new PolicyStatement({
+        actions,
+        resources,
+        sid: name,
+      }));
+    }
+  }
+
+  /**
+   * Create an account-wide OIDC connection fo Guthub Actions.
+   *
+   * NB only one OIDC provider for GitHub can be created per AWS account (because the provider URL must be unique).
+   *
+   * To provide access to resources, you can create multiple roles that trust the provider so you'll probably want to call ghaOidcRole() instead.
+   * See: https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services
+   * @param repo What to grant access to. This is a minimum of a GitHub owner (user or org), optionally a repository name, and you can also specify a filter to limit access to e.g. a branch.
+   */
+  ghaOidcProvider(): OpenIdConnectProvider {
+    return new OpenIdConnectProvider(this, 'oidc-provider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: ['sts.amazonaws.com'],
+    });
+  }
+
+  /**
+   * Add permissions to the GitHub OIDC role that allow workflows to access the AWS resources in this stack that need to be updated at build time.
+   * See: https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services
+   * @param repo The repository to grant access to (owner and name). You can also specify a filter to limit access e.g. to a branch.
+   */
+  ghaOidcRole(repo: { owner: string, repo?: string; filter?: string; }, openIdConnectProvider?: OpenIdConnectProvider): Role {
+    const provider = openIdConnectProvider || OpenIdConnectProvider.fromOpenIdConnectProviderArn(this, `oidc-provider-${Stack.of(this).account}`, `arn:aws:iam::${Stack.of(this).account}}:oidc-provider/token.actions.githubusercontent.com`);
+
+    // Grant only requests coming from the specific owner/repository/filter to assume this role.
+    const role = new Role(this, `gha-oidc-role-${this.stackName}`, {
+      assumedBy: new WebIdentityPrincipal(
+        provider.openIdConnectProviderArn,
+        {
+          StringLike: {
+            'token.actions.githubusercontent.com:sub': [`repo:${repo.owner}/${repo.repo}:${repo.filter || '*'}`],
+          },
+        },
+      ),
+      managedPolicies: [
+        this.ghaPolicy(),
+      ],
+      roleName: `gha-oidc-${this.stackName}`,
+      description: `Role for GitHub Actions to assume when deploying to ${this.stackName}`,
+    });
+    this.addGhaVariable(this, 'ghaOidc', 'Role', role.roleArn);
+
+    this.saveGhaValues();
+    return role;
+  }
+
+  /**
+ * A user for Gihud Actions CI/CD.
+ */
+  ghaUser(scope: Construct, username?: string): { user: User, accessKey: CfnAccessKey | undefined; } {
+    // A user with the policy attached
+    const user = new User(this, 'ghaUser', { userName: username || `gha-${this.stackName}` });
+    const policy = this.ghaPolicy();
+    user.addManagedPolicy(policy);
+
+    // Credentials
+    let accessKey: CfnAccessKey | undefined;
+    if (!process.env.REKEY) {
+      accessKey = new CfnAccessKey(this, 'ghaUserAccessKey', {
+        userName: user.userName,
+      });
+
+      // Access key details for GHA secrets
+      this.addGhaSecret('awsAccessKeyId', accessKey.ref);
+      this.addGhaSecret('awsSecretAccessKey', accessKey.attrSecretAccessKey);
+    }
+
+    this.saveGhaValues();
+    return { user, accessKey };
+  }
+
+  saveGhaValues() {
+    if (fs.existsSync('secrets')) {
+      // Write out the list of secret and variable names:
+      fs.writeFileSync(`secrets/${this.stackName}.ghaSecrets.json`, JSON.stringify(this.ghaInfo.secrets));
+      fs.writeFileSync(`secrets/${this.stackName}.ghaVariables.json`, JSON.stringify(this.ghaInfo.variables));
+    }
+
+    // Flush ghaInfo so we're free to build another stack if needed:
+    this.ghaInfo.resources.buckets = [];
+    this.ghaInfo.resources.distributions = [];
+    this.ghaInfo.resources.lambdas = [];
+    this.ghaInfo.resources.repositories = [];
+    this.ghaInfo.resources.services = [];
+    this.ghaInfo.secrets = [];
+    this.ghaInfo.variables = [];
+  }
+}
