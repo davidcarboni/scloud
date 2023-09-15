be8159c5df6c710cc9a4d8c05ffe3a22b34b5fb8
David <david.carboni@policyinpractice.co.uk>
2023-09-15 15:58:08 +0100

    Rework to call GithubActions So we don't end up with a split between two different permission policies

--- cdk-patterns/src/ghaUserDeprecated.ts
+++ cdk-patterns/src/ghaUserDeprecated.ts
@@ -1,28 +1,27 @@
 /* eslint-disable no-param-reassign */
-import * as fs from 'fs';
 import {
-  CfnAccessKey, ManagedPolicy, OpenIdConnectProvider, PolicyStatement, Role, User, WebIdentityPrincipal,
+  CfnAccessKey, OpenIdConnectProvider, Role, User,
 } from 'aws-cdk-lib/aws-iam';
-import { CfnOutput, Stack } from 'aws-cdk-lib';
+import { Stack } from 'aws-cdk-lib';
 import { IRepository } from 'aws-cdk-lib/aws-ecr';
 import { IFunction } from 'aws-cdk-lib/aws-lambda';
-import { IFargateService } from 'aws-cdk-lib/aws-ecs';
 import { IBucket } from 'aws-cdk-lib/aws-s3';
 import { IDistribution } from 'aws-cdk-lib/aws-cloudfront';
 import { Construct } from 'constructs';
 import _ from 'lodash';
+import { githubActions } from './GithubActions';
 
-const ghaInfo = {
-  resources: {
-    repositories: <IRepository[]>[],
-    buckets: <IBucket[]>[],
-    lambdas: <IFunction[]>[],
-    services: <IFargateService[]>[],
-    distributions: <IDistribution[]>[],
-  },
-  secrets: <string[]>[],
-  variables: <string[]>[],
-};
+// const ghaInfo = {
+//   resources: {
+//     repositories: <IRepository[]>[],
+//     buckets: <IBucket[]>[],
+//     lambdas: <IFunction[]>[],
+//     services: <IFargateService[]>[],
+//     distributions: <IDistribution[]>[],
+//   },
+//   secrets: <string[]>[],
+//   variables: <string[]>[],
+// };
 
 // @deprecated - Use GithubActions instead
 export function addGhaSecret(
@@ -30,8 +29,7 @@ export function addGhaSecret(
   name: string,
   value: string,
 ) {
-  const cfnOutput = new CfnOutput(construct, name, { value });
-  ghaInfo.secrets.push(cfnOutput.node.id);
+  githubActions(construct).addGhaSecret(name, value);
 }
 
 // @deprecated - Use GithubActions instead
@@ -41,9 +39,7 @@ export function addGhaVariable(
   type: string,
   value: string,
 ) {
-  const variableName = `${_.lowerFirst(name)}${_.capitalize(type)}`;
-  const cfnOutput = new CfnOutput(construct, variableName, { value });
-  ghaInfo.variables.push(cfnOutput.node.id);
+  githubActions(construct).addGhaVariable(name, type, value);
 }
 
 // @deprecated - Use GithubActions instead
@@ -52,8 +48,7 @@ export function addGhaLambda(
   name: string,
   lambda: IFunction,
 ) {
-  ghaInfo.resources.lambdas.push(lambda);
-  addGhaVariable(construct, name, 'lambda', lambda.functionName);
+  githubActions(construct).addGhaLambda(name, lambda);
 }
 
 // @deprecated - Use GithubActions instead
@@ -62,8 +57,7 @@ export function addGhaBucket(
   name: string,
   bucket: IBucket,
 ) {
-  ghaInfo.resources.buckets.push(bucket);
-  addGhaVariable(construct, name, 'bucket', bucket.bucketName);
+  githubActions(construct).addGhaBucket(name, bucket);
 }
 
 // @deprecated - Use GithubActions instead
@@ -72,8 +66,7 @@ export function addGhaDistribution(
   name: string,
   distribution: IDistribution,
 ) {
-  ghaInfo.resources.distributions.push(distribution);
-  addGhaVariable(construct, name, 'distributionId', distribution.distributionId);
+  githubActions(construct).addGhaDistribution(name, distribution);
 }
 
 // @deprecated - Use GithubActions instead
@@ -82,109 +75,18 @@ export function addGhaRepository(
   name: string,
   repository: IRepository,
 ) {
-  ghaInfo.resources.repositories.push(repository);
-  addGhaVariable(construct, name, 'repository', repository.repositoryName);
+  githubActions(construct).addGhaRepository(name, repository);
 }
 
 // @deprecated - Use GithubActions instead
 export function saveGhaValues(stack: Stack) {
-  if (fs.existsSync('secrets')) {
-    // Write out the list of secret and variable names:
-    fs.writeFileSync(`secrets/${stack.stackName}.ghaSecrets.json`, JSON.stringify(ghaInfo.secrets));
-    fs.writeFileSync(`secrets/${stack.stackName}.ghaVariables.json`, JSON.stringify(ghaInfo.variables));
-  }
-
-  // Flush ghaInfo so we're free to build another stack if needed:
-  ghaInfo.resources.buckets = [];
-  ghaInfo.resources.distributions = [];
-  ghaInfo.resources.lambdas = [];
-  ghaInfo.resources.repositories = [];
-  ghaInfo.resources.services = [];
-  ghaInfo.secrets = [];
-  ghaInfo.variables = [];
+  githubActions(stack).saveGhaValues();
 }
 
 // @deprecated - Use GithubActions instead
-function addToPolicy(stack: Stack, name: string, policy: ManagedPolicy, resources: string[], actions: string[]) {
-  if (resources.length > 0) {
-    policy.addStatements(new PolicyStatement({
-      actions,
-      resources,
-      sid: name,
-    }));
-  }
-}
-
-// @deprecated - Use GithubActions instead
+// eslint-disable-next-line no-unused-vars
 export function ghaPolicy(stack: Stack, name: string = `gha-${stack.stackName}-policy`) {
-  const policy = new ManagedPolicy(stack, name, {
-    managedPolicyName: name,
-  });
-
-  // ECR repositories - push/pull images
-  const repositoryArns = ghaInfo.resources.repositories
-    .filter((repository) => repository)
-    .map((repository) => repository.repositoryArn);
-  if (repositoryArns.length > 0) addToPolicy(stack, 'ecrLogin', policy, ['*'], ['ecr:GetAuthorizationToken']);
-  addToPolicy(stack, 'ecrRepositories', policy, repositoryArns, [
-    'ecr:GetDownloadUrlForLayer',
-    'ecr:BatchGetImage',
-    'ecr:BatchDeleteImage',
-    'ecr:CompleteLayerUpload',
-    'ecr:UploadLayerPart',
-    'ecr:InitiateLayerUpload',
-    'ecr:BatchCheckLayerAvailability',
-    'ecr:PutImage',
-    'ecr:ListImages',
-  ]);
-
-  // Buckets - upload/sync
-  const bucketArns = ghaInfo.resources.buckets
-    .filter((bucket) => bucket)
-    .map((bucket) => bucket.bucketArn);
-  addToPolicy(stack, 'buckets', policy, bucketArns, [
-    's3:ListBucket',
-  ]);
-  const objectArns = bucketArns.map((arn) => `${arn}/*`);
-  addToPolicy(stack, 'bucketObjects', policy, objectArns, [
-    's3:GetObject',
-    's3:PutObject',
-    's3:DeleteObject',
-  ]);
-
-  // Lambdas - update update with a new zip/container build
-  const lambdaArns = ghaInfo.resources.lambdas
-    .filter((lambda) => lambda)
-    .map((lambda) => lambda.functionArn);
-  addToPolicy(stack, 'lambdas', policy, lambdaArns, [
-    'lambda:UpdateFunctionCode',
-    // 'lambda:PublishVersion',
-  ]);
-
-  // Fargate services - update with a new container build
-  const serviceArns = ghaInfo.resources.services
-    .filter((service) => service)
-    .map((service) => service.serviceArn);
-  addToPolicy(stack, 'fargateServices', policy, serviceArns, [
-    'ecs:UpdateService',
-  ]);
-
-  // Cloudfront distribution - cache invalidation
-  const distributionArns = ghaInfo.resources.distributions
-    .filter((distribution) => distribution !== undefined)
-    // Not sure where to 'properly' get a distribution ARN from?
-    .map((distribution) => `arn:aws:cloudfront::${stack.account}:distribution/${distribution.distributionId}`);
-  addToPolicy(stack, 'distributions', policy, distributionArns, [
-    'cloudfront:CreateInvalidation',
-  ]);
-
-  if (fs.existsSync('secrets')) {
-    // Write out the list of secret and variable names:
-    fs.writeFileSync(`secrets/${stack.stackName}.ghaSecrets.json`, JSON.stringify(ghaInfo.secrets));
-    fs.writeFileSync(`secrets/${stack.stackName}.ghaVariables.json`, JSON.stringify(ghaInfo.variables));
-  }
-
-  return policy;
+  return githubActions(stack).ghaPolicy();
 }
 
 /**
@@ -197,10 +99,7 @@ export function ghaPolicy(stack: Stack, name: string = `gha-${stack.stackName}-p
  * @param repo What to grant access to. This is a minimum of a GitHub owner (user or org), optionally a repository name, and you can also specify a filter to limit access to e.g. a branch.
  */
 export function ghaOidcProvider(stack: Stack): OpenIdConnectProvider {
-  return new OpenIdConnectProvider(stack, 'oidc-provider', {
-    url: 'https://token.actions.githubusercontent.com',
-    clientIds: ['sts.amazonaws.com'],
-  });
+  return githubActions(stack).ghaOidcProvider();
 }
 
 /**
@@ -211,28 +110,7 @@ export function ghaOidcProvider(stack: Stack): OpenIdConnectProvider {
  * @param repo The repository to grant access to (owner and name). You can also specify a filter to limit access e.g. to a branch.
  */
 export function ghaOidcRole(stack: Stack, repo: { owner: string, repo?: string; filter?: string; }, openIdConnectProvider?: OpenIdConnectProvider): Role {
-  const provider = openIdConnectProvider || OpenIdConnectProvider.fromOpenIdConnectProviderArn(stack, `gha-oidc-${stack.account}`, `arn:aws:iam::${stack.account}:oidc-provider/token.actions.githubusercontent.com`);
-
-  // Grant only requests coming from the specific owner/repository/filter to assume this role.
-  const role = new Role(stack, `gha-oidc-role-${stack.stackName}`, {
-    assumedBy: new WebIdentityPrincipal(
-      provider.openIdConnectProviderArn,
-      {
-        StringLike: {
-          'token.actions.githubusercontent.com:sub': [`repo:${repo.owner}/${repo.repo}:${repo.filter || '*'}`],
-        },
-      },
-    ),
-    managedPolicies: [
-      ghaPolicy(stack),
-    ],
-    roleName: `gha-oidc-${stack.stackName}`,
-    description: `Role for GitHub Actions (${stack.stackName}) to assume when deploying to AWS`,
-  });
-  addGhaVariable(stack, 'ghaOidc', 'Role', role.roleArn);
-
-  saveGhaValues(stack);
-  return role;
+  return githubActions(stack).ghaOidcRole(repo, openIdConnectProvider);
 }
 
 /**
@@ -241,23 +119,5 @@ export function ghaOidcRole(stack: Stack, repo: { owner: string, repo?: string; 
  * A user for Gihud Actions CI/CD.
  */
 export function ghaUser(stack: Stack, username?: string): { user: User, accessKey: CfnAccessKey | undefined; } {
-  // A user with the policy attached
-  const user = new User(stack, 'ghaUser', { userName: username || `gha-${stack.stackName}` });
-  const policy = ghaPolicy(stack);
-  user.addManagedPolicy(policy);
-
-  // Credentials
-  let accessKey: CfnAccessKey | undefined;
-  if (!process.env.REKEY) {
-    accessKey = new CfnAccessKey(stack, 'ghaUserAccessKey', {
-      userName: user.userName,
-    });
-
-    // Access key details for GHA secrets
-    addGhaSecret(stack, 'awsAccessKeyId', accessKey.ref);
-    addGhaSecret(stack, 'awsSecretAccessKey', accessKey.attrSecretAccessKey);
-  }
-
-  saveGhaValues(stack);
-  return { user, accessKey };
+  return githubActions(stack).ghaUser(username);
 }
