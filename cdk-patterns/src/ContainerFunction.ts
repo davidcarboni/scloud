dbb75c1506aa84c144f693013e01fef12654e5ab
David Carboni <david@carboni.io>
2023-09-15 21:39:33 +0100

    Modified to directly extend DockerImageFunction

--- cdk-patterns/src/ContainerFunction.ts
+++ cdk-patterns/src/ContainerFunction.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 import {
-  DockerImageCode, DockerImageFunction, DockerImageFunctionProps,
+  DockerImageCode, DockerImageFunction, DockerImageFunctionProps, Function,
 } from 'aws-cdk-lib/aws-lambda';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { EcrRepository } from './EcrRepository';
@@ -13,17 +13,18 @@ import { githubActions } from './GithubActions';
  *
  * This construct automatically adds itself to the list of resources Github Actions needs to access.
  *
+ * NB when a ContainerFunction is first built you'll need to set initialPass to true.
+ * This is because the ECR repository needs to be created before the container image can be pushed to it.
+ * This construct will fail to build if there is no image in the ECR repository.
+ *
  * @param construct Parent CDK construct (typically 'this')
- * @param initialPass If the infrastructure is being built from scratch: true;
- * for incremental deployments: false.
+ * @param initialPass If the infrastructure is being built from scratch: true, for incremental deployments: false.
  * @param name The name for this function
  * @param environment Environment variables for the Lambda function
  * @returns The lambda, if created, and associated ECR repository
  */
-export class ContainerFunction extends Construct {
+export class ContainerFunction extends DockerImageFunction {
   repository: Repository;
-
-  lambda: DockerImageFunction;
 
   constructor(
     scope: Construct,
@@ -34,22 +35,26 @@ export class ContainerFunction extends Construct {
     ecr?: Repository,
     initialPass: boolean = false,
   ) {
-    super(scope, `${id}ContainerFunction`);
     // Repository for function container image
-    this.repository = ecr || new EcrRepository(scope, `${id}Repository`);
+    // NB this will be created on the initial pass
+    // It then needs to be populated with an image
+    // After that the image can be referenced by this construct
+    const repository = ecr || new EcrRepository(scope, `${id}Repository`);
 
     // Container
-    const code = initialPass ? DockerImageCode.fromImageAsset(path.join(__dirname, './container')) : DockerImageCode.fromEcr(this.repository, {
+    const code = initialPass ? DockerImageCode.fromImageAsset(path.join(__dirname, './container')) : DockerImageCode.fromEcr(repository, {
       tagOrDigest: tagOrDigest || 'latest',
     });
 
-    this.lambda = new DockerImageFunction(scope, id, {
+    super(scope, `${id}ContainerFunction`, {
       code,
       logRetention: logs.RetentionDays.THREE_MONTHS,
       environment,
       description: id,
       ...props,
     });
-    githubActions(scope).addGhaLambda(id, this.lambda);
+
+    this.repository = repository;
+    githubActions(scope).addGhaLambda(id, this);
   }
 }
