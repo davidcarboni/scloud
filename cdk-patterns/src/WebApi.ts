bdaf5a965591c9ff6b88ed40ff14c2406613e28b
David Carboni <david@carboni.io>
2024-02-27 12:48:15 +0000

    Simplify and improve documentation comment

--- cdk-patterns/src/WebApi.ts
+++ cdk-patterns/src/WebApi.ts
@@ -18,10 +18,9 @@ import {
 import { Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { ARecord, IHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { RedirectWww } from './RedirectWww';
 import { githubActions } from './GithubActions';
 import { PrivateBucket } from './PrivateBucket';
-import { ZipFunction, ZipFunctionProps } from './ZipFunction';
+
 /**
  * @param lambda The function which will respond to incoming request events.
  * @param zone The DNS zone for this web app.
@@ -33,15 +32,15 @@ export interface WebApiProps {
   lambda: Function,
   zone: IHostedZone,
   domainName?: string,
-  defaultIndex?: boolean,
-  redirectWww?: boolean,
   distributionProps?: Partial<DistributionProps>,
 }
 
 /**
- * Builds a dynamic web application, backed by a single Lambda function, also knowm as a "Lambda-lith" (https://github.com/cdk-patterns/serverless/blob/main/the-lambda-trilogy/README.md)
+ * Builds a web API, backed by a single Lambda function - a kind of "Lambda-lith" (https://github.com/cdk-patterns/serverless/blob/main/the-lambda-trilogy/README.md)
  *
  * This construct sends requests that don't have a file extension to the Lambda. Static content is handled by routing requests that match *.* (eg *.js. *.css) to an S3 bucket.
+ * Dumping requests with file expensions means the majority of spam requests will not invoke your Lambda.
+ * Typically spam requests are probing for wordpress *.xml files, *.php files, .env files etc.
  */
 export class WebApi extends Construct {
   lambda: Function;
@@ -87,7 +86,6 @@ export class WebApi extends Construct {
       domainName,
       hostedZone: props.zone,
       region: 'us-east-1',
-      subjectAlternativeNames: props.redirectWww !== false ? [`www.${domainName}`] : undefined,
     });
 
     // This enables us to separate out the defaultBehavior props (if any) from the distributionProps (if provided)
@@ -96,7 +94,6 @@ export class WebApi extends Construct {
     this.distribution = new Distribution(scope, `${id}Distribution`, {
       domainNames: [domainName],
       comment: domainName,
-      defaultRootObject: props.defaultIndex ? 'index.html' : undefined,
       defaultBehavior: {
         origin: new RestApiOrigin(this.api),
         allowedMethods: AllowedMethods.ALLOW_ALL,
@@ -126,48 +123,6 @@ export class WebApi extends Construct {
       recordName: domainName,
       target: RecordTarget.fromAlias(new CloudFrontTarget(this.distribution)),
       zone: props.zone,
-    });
-
-    if (props.redirectWww !== false) new RedirectWww(scope, id, { zone: props.zone, certificate: this.certificate, domainName });
-  }
-
-  /**
-   * Creates a WebApi backed by a Node.js Lambda function.
-   *
-   * Memory defaults to 3008 MB because this has the effest of assigning more compute resource and therefore reduces latency.
-   */
-  static node(
-    scope: Construct,
-    id: string,
-    zone: IHostedZone,
-    domainName?: string,
-    defaultIndex?: boolean,
-    redirectWww?: boolean,
-    functionProps?: ZipFunctionProps,
-  ): WebApi {
-    const lambda = ZipFunction.node(scope, id, { memorySize: 3008, ...functionProps });
-    return new WebApi(scope, id, {
-      lambda, zone, domainName, defaultIndex, redirectWww,
-    });
-  }
-
-  /**
-   * Creates a WebApi backed by a Python Lambda function.
-   *
-   * Memory defaults to 3008 MB because this has the effest of assigning more compute resource and therefore reduces latency.
-   */
-  static python(
-    scope: Construct,
-    id: string,
-    zone: IHostedZone,
-    domainName?: string,
-    defaultIndex?: boolean,
-    redirectWww?: boolean,
-    functionProps?: ZipFunctionProps,
-  ): WebApi {
-    const lambda = ZipFunction.python(scope, id, { memorySize: 3008, ...functionProps });
-    return new WebApi(scope, id, {
-      lambda, zone, domainName, defaultIndex, redirectWww,
     });
   }
 }
