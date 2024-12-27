3db42709e92383393d74750fce1db39ae4238654
David Carboni <david@carboni.io>
2024-12-27 17:45:30 +0000

    Lint

--- s3/test/placeholder.spec.ts
+++ s3/test/placeholder.spec.ts
@@ -5,6 +5,7 @@ import { describe, it } from 'mocha';
 
 describe('s3.ts', () => {
   it('This package contains no tests because there\'s no testable code - these are just wrapper funcitons for aws sdk v3.', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     expect(true).to.be.true;
   });
 });
