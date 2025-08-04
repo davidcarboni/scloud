27e59c62af14fc0a2bdcaf2b26e7290eafd4c3e8
David Carboni <david@carboni.io>
2025-08-04 14:37:51 +0100

    Update eslint config

---
+++ lambda-api/eslint.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import noOnlyTests from 'eslint-plugin-no-only-tests';
+
+// https://typescript-eslint.io/getting-started/
+
+const ignores = [
+  '**/*.js',
+];
+
+export default tseslint.config(
+  {
+    ...eslint.configs.recommended,
+    ignores,
+  },
+  ...tseslint.configs.recommended.map((config) => ({
+    ...config,
+    ignores,
+  })),
+  {
+    files: ['test/**/*.spec.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-only-tests/no-only-tests': 'error',
+    },
+    plugins: {
+      'no-only-tests': noOnlyTests,
+    },
+  },
+);
