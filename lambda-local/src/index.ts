e95a30d20eb1e8ba2835fa15318b78af923ceb98
David Carboni <david@carboni.io>
2025-09-17 10:45:17 +0100

    Add exports

--- lambda-local/src/index.ts
+++ lambda-local/src/index.ts
@@ -2,3 +2,5 @@ export { sqsLocal } from './sqsLocal';
 export { cloudfrontLocal } from './cloudfrontLocal';
 export { webappLocal } from './webappLocal';
 export { webappRoutesLocal } from './webappRoutesLocal';
+export { webApiLocal } from './webApiLocal';
+export { scheduledLocal } from './scheduledLocal';
