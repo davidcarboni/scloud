e84b26456dedadb49102c6ef13baea130ab4bdf6
David Carboni <david@carboni.io>
2026-04-18 22:59:07 +0100

    Sorting out eslint and dependencies, removing package-locks from the repo

--- lambda-local/eslint.config.mjs
+++ lambda-local/eslint.config.mjs
@@ -1,38 +1,13 @@
-import eslint from '@eslint/js';
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import noOnlyTests from 'eslint-plugin-no-only-tests';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://typescript-eslint.io/getting-started/
-
-const ignores = [
-  '**/*.js',
-  'bundle.ts',
-];
-
-export default [
-  {
-    ...eslint.configs.recommended,
-    ignores,
-  },
-  ...tseslint.configs.recommended.map((config) => ({
-    ...config,
-    ignores,
-  })),
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ['./tsconfig.eslint.json'],
-        tsconfigRootDir: __dirname,
-      },
-    },
-  },
+export default defineConfig(
+  js.configs.recommended,
+  tseslint.configs.recommended,
   {
     files: ['**/*.spec.ts'],
     rules: {
@@ -44,4 +19,4 @@ export default [
       'no-only-tests': noOnlyTests,
     },
   },
-];
+);
