2b5c1f3a1bd1d4e82107919e5bd0b20cce78a8c8
lambda-queue <gha@scloud/lambda-queue>
2026-06-18 22:52:42 +0000

    Patch version

--- lambda-queue/src/handler.ts
+++ lambda-queue/src/handler.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-/* eslint-disable no-unused-vars */
+ 
 import {
   Context, SQSBatchResponse, SQSEvent, SQSRecord,
 } from 'aws-lambda';
--- lambda-queue/test/handler.spec.ts
+++ lambda-queue/test/handler.spec.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+ 
 import {
   Context, SQSBatchResponse, SQSEvent, SQSRecord,
 } from 'aws-lambda';
